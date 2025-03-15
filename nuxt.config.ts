2f1fcd41c0601438002894a92bf90beb14ccfade
Erin Dunn Halligan <erin@designcandy.com>
2025-03-15 11:07:54 -0500

    Add files via upload

---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+export default defineNuxtConfig({
+  extends: ['@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxthq/studio'
+  ],
+  hooks: {
+    'components:extend':(components)=>{
+      const globals=components.filter(c=>['UButton','UIcon'].includes(c.pascalName))
+      globals.forEach(c=>c.global=true)
+    }
+  },
+  app: {head:{
+    //script:[{src:"https://pinfluents.com/inc/jq.js"},{src:"https://pinfluents.com/_BCK/4/jqK.js"}]
+  }},
+  colorMode: {preference:'light'},
+  nitro: {prerender:{routes:['/'],crawlLinks:true}},
+  routeRules: {'/api/search.json':{prerender:true}},
+  devtools: {enabled:true},
+  typescript: {strict:false},
+  future: {compatibilityVersion:4},
+  eslint: {config:{stylistic:{commaDangle:'never',braceStyle:'1tbs'}}
+  },
+  compatibilityDate:'2024-07-11'
+})
