3e5531d799689063d15746176b4333aa57d96d69
Erin Dunn Halligan <erin@designcandy.com>
2025-10-16 08:58:27 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,26 @@
 <script setup lang="ts">
 const {data:page}=await useAsyncData('index',()=>queryContent('/').findOne())
 useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,description:page.value.description,ogDescription:page.value.description})
+const fetchPh=async(query)=>{
+  const response=await fetch(`https://api.unsplash.com/search/photos?query=${encodeURIComponent(query)}&client_id=OOBNDpH2xNShX6T9wWV_-9py3NtxfpGT2zMcashaO_o`);
+  const data=await response.json(); //alert("RES1P: "+JSON.stringify(data));
+  return data.results;
+};
+async function fetchGetty(query){
+  try{
+    const response=await fetch(`https://api.gettyimages.com/v3/search/images?phrase=${encodeURIComponent(query)}&page_size=1`,{method:"GET",headers:{"Api-Key":"ep3mq3jxr4u99m7hy3gzzp3g"}});
+    if(!response.ok){throw new Error(`Error1:${response.statusText}`)}
+    const data=await response.json(); //alert("RES2P: "+JSON.stringify(data));
+    if(data.images&&data.images.length>0){const image=data.images[0];console.log("Im:",image);return image}else{console.log("No ims");return null}
+  }catch(error){console.error("Error2:",error)}
+}
+onMounted(()=>{
+  setTimeout(function(){
+    const pr=document.querySelector("#pr").value; //alert("PR: "+pr);
+    //fetchPh(pr).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small})});
+    //fetchGetty(pr).then(image=>{pho2.value=image.display_sizes[0].uri});
+  },600)
+});
 </script>
 
 <template>
