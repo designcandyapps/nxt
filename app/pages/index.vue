f2f48be229959bce14704cdb97b6ec26899e4dd9
Erin Dunn Halligan <erin@designcandy.com>
2025-11-02 14:54:57 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -26,9 +26,9 @@ onMounted(()=>{
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
-      <div id="tz" style="position:absolute; z-index:10; top:1px; left:1px; border:3px solid black;"><ImageGenerator /></div>
+      <div id="tz" style="position:absolute; z-index:10; top:1px; left:1px; border:7px solid black;"><ImageGenerator /></div>
       <div class="g">
-        <div id="z"><ColorThief /></div><input id="prompt" v-model="prompt"><input id="pho" v-model="pho"><input id="pho2" v-model="pho2">
+        <div id="z"><ColorThief /></div><input id="prompt" v-model="prompt" style="border:7px solid blue;"><input id="pho" v-model="pho"><input id="pho2" v-model="pho2">
         <div id="response" v-if="response">{{response}}</div>
       </div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
@@ -44,7 +44,7 @@ onMounted(()=>{
 <script lang="ts">
 export default{
   data(){return{prompt:"",response:null}},
-  //mounted(){setTimeout(()=>{this.send()},1300)},
+  mounted(){setTimeout(()=>{this.send()},1300)},
   methods:{
     async send(){
       const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector("#prompt").value})});
