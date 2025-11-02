a44180f2bf175a7b1bd9f6c60fef7fc03c8632f4
Erin Dunn Halligan <erin@designcandy.com>
2025-11-02 12:45:28 -0600

    Update chat.ts

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -1,4 +1,16 @@
-import {OpenAI} from "openai"
+import OpenAI from "openai";
+const client = new OpenAI({apiKey: process.env.OPENAI_API_KEY,
+});
+const main = async () => {
+  const response = await client.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [{ role: "user", content: "Hello OpenAI!" }],
+  });
+  alert(response.choices[0].message.content);
+};
+main();
+
+/*import {OpenAI} from "openai"
 export default defineEventHandler(async(event)=>{
   const body=await readBody(event)
   const client=new OpenAI({apiKey:process.env.OPENAI_API_KEY})
@@ -7,4 +19,4 @@ export default defineEventHandler(async(event)=>{
     messages:body.messages,
   })
   return completion.choices[0].message
-})
+})*/
