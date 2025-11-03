2a0f1aca4b740dc2238e1426640358ee46a37449
Erin Dunn Halligan <erin@designcandy.com>
2025-11-03 11:30:39 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -47,7 +47,7 @@ onMounted(()=>{
 <script lang="ts">
 export default{
   data(){return{prompt:"",response:null}},
-  mounted(){setTimeout(()=>{this.send()},1300)},
+  mounted(){setTimeout(()=>{this.send()},300)},
   methods:{
     async send(){
       const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector("#prompt").value})});
