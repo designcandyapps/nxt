8033f857eca9e81572732a22274200895af3c640
Erin Dunn Halligan <erin@designcandy.com>
2025-11-03 11:27:49 -0600

    Update ImageGenerator.vue

--- app/components/ImageGenerator.vue
+++ app/components/ImageGenerator.vue
@@ -2,7 +2,9 @@
 <script>
 export default{
   mounted(){setTimeout(()=>{this.generateImage()},300)},
-  setup(){const prompt=ref(""); const image=ref("");
+  setup(){
+   setTimeout(()=>{
+    const prompt=ref(""); const image=ref("");
     const generateImage=async()=>{image.value="";
       const response=await fetch("https://api.openai.com/v1/images/generations",{
         method:"POST",headers:{"Content-Type":"application/json",
@@ -12,6 +14,7 @@ export default{
       const data=await response.json(); alert("RES2Ab: "+JSON.stringify(data));
       if(data&&data.data&&data.data.length>0){image.value=data.data[0].url;
       document.querySelector("#v").firstChild.style.backgroundImage="url("+image.value+")";}
+   },2300);
     }
     return{prompt,image,generateImage};
   },
