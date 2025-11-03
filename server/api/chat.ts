8799c6b665639a559e24fc6d607eea7e6edcb5c8
Erin Dunn Halligan <erin@designcandy.com>
2025-11-03 11:23:19 -0600

    Update chat.ts

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -5,7 +5,8 @@ export default defineEventHandler(async(event)=>{
     method:"POST",
     headers:{
       "Content-Type": "application/json",
-      Authorization: `Bearer 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`,
+      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+      //"Authorization": `Bearer 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`,
     },
     body:JSON.stringify({
       model: "gpt-3.5-turbo",
