04ce09a6234a21ff4028e80167bb4e609d5881e4
Erin Dunn Halligan <erin@designcandy.com>
2025-11-03 21:23:06 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -51,7 +51,7 @@ export default{
     async send(){
       const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector("#prompt").value})});
       const data=await response.json(); this.response=data.reply; alert("RES00: "+JSON.stringify(data)); //alert("RES01: "+this.response);
-      //document.querySelector("#tz").innerText=this.response;
+      document.querySelector("#tz").innerText=this.response;
     },
   },
 }
