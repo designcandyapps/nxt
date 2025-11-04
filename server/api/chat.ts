30d7a7b76d63d9913b0d1cc992ea6dfacdd6faf0
Erin Dunn Halligan <erin@designcandy.com>
2025-11-03 21:37:24 -0600

    Update chat.ts

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 const openai=new OpenAI({
-  apiKey: "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",
+  apiKey:`Bearer ${process.env.OPENAI_API_KEY}`
 });
 const response=openai.responses.create({
   model:"gpt-5-nano",
@@ -8,3 +8,5 @@ const response=openai.responses.create({
   store:true,
 });
 response.then((result)=>console.log(result.output_text));
+
+
