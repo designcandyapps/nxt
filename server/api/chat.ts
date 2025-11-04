3da6f47923f3de908ef2e6f2ebc22c388278a433
Erin Dunn Halligan <erin@designcandy.com>
2025-11-04 04:57:01 -0600

    Update chat.ts

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -14,32 +14,3 @@ export default defineEventHandler(async(event)=>{
   const data=await apiResponse.json();
   return{reply:data.choices[0].message.content};
 });
-
-
-
-/*import {OpenAI} from "openai";
-export default defineEventHandler(async(event)=>{
-  const body=await readBody(event);
-  const client=new OpenAI({apiKey:document.querySelector("#apiKey").value});
-  alert("CL: "+client);
-  const completion=await client.chat.completions.create({
-    model:"gpt-4o",
-    messages:body.messages,
-  });
-  return completion.choices[0].message;
-});
-
-
-import OpenAI from "openai";
-alert("AK: "+document.querySelector("#apiKey").value);
-const openai=new OpenAI({
-  //apiKey:apiKey.value
-  apiKey:document.querySelector("#apiKey").value
-});
-const response=openai.responses.create({
-  model:"gpt-5-nano",
-  input:"write a haiku",
-  store:true,
-});
-response.then((result)=>console.log(result.output_text));
-*/
