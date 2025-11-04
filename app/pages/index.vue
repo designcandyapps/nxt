0a0d8e5bbaf94937a120c88296a07df5daec406f
Erin Dunn Halligan <erin@designcandy.com>
2025-11-03 22:20:45 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -31,6 +31,7 @@ onMounted(()=>{
         <div id="z"><ColorThief /></div><input id="prompt" v-model="prompt" style="border:3px solid blue;">
         <input id="pho" v-model="pho" style="border:3px solid green;">
         <input id="pho2" v-model="pho2" style="border:3px solid orange;">
+        <input id="apiKey" v-model="apiKey" style="border:3px solid navy;">
         <div id="response" v-if="response" style="border:3px solid yellow;">{{response}}</div><ImageGenerator />
       </div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
