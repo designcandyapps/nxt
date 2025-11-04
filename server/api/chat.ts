fefc63722c145da348b2c443dcfcbf27ee36f44a
Erin Dunn Halligan <erin@designcandy.com>
2025-11-03 19:14:54 -0600

    Update chat.ts

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -3,8 +3,8 @@ export default defineEventHandler(async(event)=>{
   const apiResponse=await fetch("https://api.openai.com/v1/responses",{
     method:"POST",
     headers:{
-      "Content-Type": "application/json",
-      "Authorization": `Bearer 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`
+      "Content-Type":"application/json",
+      "Authorization":`Bearer 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`
     },
     body:JSON.stringify({
       model: "gpt-5-nano",
