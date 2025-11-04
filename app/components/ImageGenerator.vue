6594b3042435c76b64fcaea11a7ea8782a8defd8
Erin Dunn Halligan <erin@designcandy.com>
2025-11-03 18:08:27 -0600

    Update ImageGenerator.vue

--- app/components/ImageGenerator.vue
+++ app/components/ImageGenerator.vue
@@ -7,7 +7,7 @@ export default{
     const generateImage=async()=>{image.value="";
       const response=await fetch("https://api.openai.com/v1/images/generations",{
         method:"POST",headers:{"Content-Type":"application/json",
-        Authorization:`Bearer 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`
+        Authorization:`Bearer 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`
       },
       body:JSON.stringify({prompt:document.querySelector("#prompt").value,n:1,size:"256x256"})});
       const data=await response.json(); alert("RES2Ab: "+JSON.stringify(data));
