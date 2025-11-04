61b92084586bfeb6289b40953fb827076225b339
Erin Dunn Halligan <erin@designcandy.com>
2025-11-03 18:59:58 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -31,7 +31,7 @@ async function fetchGetty(query){
         <div id="z"><ColorThief /></div><input id="prompt" v-model="prompt" style="border:3px solid blue;">
         <input id="pho" v-model="pho" style="border:3px solid green;">
         <input id="pho2" v-model="pho2" style="border:3px solid orange;">
-        <div id="response" v-if="response" style="border:3px solid yellow;">{{response}}</div><ImageGenerator />
+        <div id="response" v-if="response" style="border:3px solid yellow;">{{response}}</div>
       </div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
     </ULandingHero>
@@ -51,7 +51,7 @@ export default{
     async send(){
       const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector("#prompt").value})});
       const data=await response.json(); this.response=data.reply; alert("RES00: "+JSON.stringify(data)); //alert("RES01: "+this.response);
-      document.querySelector("#tz").innerText=this.response;
+      //document.querySelector("#tz").innerText=this.response;
     },
   },
 }
