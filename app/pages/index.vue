64bfca0d2e6c49f2f8fe47324e0b1750877658e5
Erin Dunn Halligan <erin@designcandy.com>
2025-11-07 09:36:34 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -28,6 +28,7 @@ onMounted(()=>{
     <ULandingHero v-if="page.hero" v-bind="page.hero">
       <div class="g">
         <div id="z"><ColorThief /></div><input id="prompt" v-model="prompt"><input id="pho" v-model="pho"><input id="pho2" v-model="pho2">
+        <input id="pr2" v-model="pr2"><br><input id="pr3" v-model="pr3"><br><input id="pr4" v-model="pr4"><br><<input id="pr5" v-model="pr5">
         <div id="response" v-if="response">{{response}}</div>
       </div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
