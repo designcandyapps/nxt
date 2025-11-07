37329129afa501c48d805f7e6231f704064008f7
Erin Dunn Halligan <erin@designcandy.com>
2025-11-07 14:09:10 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -52,7 +52,7 @@ export default{
   data(){return{prompt:"",response:null}},
   mounted(){
     setTimeout(()=>{this.send()},7800);
-    setTimeout(()=>{this.send2()},19800);
+    setTimeout(()=>{this.send2()},9800);
   },
   methods:{
     async send(){
