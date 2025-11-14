63e1fc147ab8cd63979b251dd0e6b76a95ae04f5
Erin Dunn Halligan <erin@designcandy.com>
2025-11-13 20:05:42 -0600

    Create Nav.vue

---
+++ app/components/Nav.vue
@@ -0,0 +1,30 @@
+<header>
+  <div>
+    <div>
+
+      <div>
+        <div></div>
+      </div>
+
+      <div>
+        <div>
+          <ul>
+            <li><a>Learn</a>
+              <ul>
+                <li><a href="#">Practice</a></li>
+                <li><a href="#">Product</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+    </div>
+  </div>
+</header>
