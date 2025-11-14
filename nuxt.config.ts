9bcc7cabba28ecf8bf66b8cc46ffc5e6368ef0a3
Erin Dunn Halligan <erin@designcandy.com>
2025-11-13 21:21:57 -0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
       {rel:'stylesheet',href:'https://pinfluents.com/_BCK/4/inc/ct.css'},
     ],
     script:[
-      //{src:'https://pinfluents.com/inc/jq.js'},
+      {src:'https://pinfluents.com/inc/jq.js'},
       //{src:'https://pinfluents.com/_BCK/4/inc/sl.js'},
 
       {src:'https://pinfluents.com/inc/jq3.7.js'},
