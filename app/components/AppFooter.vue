df5fd69560cbad14c96fce3cf6d0d7591d009ef0
Erin Dunn Halligan <erin@designcandy.com>
2025-11-14 08:42:21 -0600

    Update AppFooter.vue

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
-const { footer }=useAppConfig()
+const {footer}=useAppConfig()
 </script>
 
 <template>
   <UFooter>
-    <template #left>{{ footer.credits }}</template>
+    <template #left>
+      Test
+    </template>
     <template #right>
-      <UColorModeButton v-if="footer?.colorMode" />
-      <template v-if="footer?.links">
-        <UButton v-for="(link,index) of footer?.links" key="index" v-bind="{ color:'gray',variant:'ghost',...link }" />
-      </template>
+      <template v-if="footer?.links"><UButton v-for="(link,index) of footer?.links" key="index" v-bind="{color:'gray',variant:'ghost',...link}" /></template>
     </template>
   </UFooter>
 </template>
