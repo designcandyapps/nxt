55a0de9a8a2a4d5280f264a08f2ddd91b77a16be
Erin Dunn Halligan <erin@designcandy.com>
2025-11-17 14:44:08 -0600

    Create ab.vue

---
+++ app/components/ab.vue
@@ -0,0 +1,7 @@
+<section class="section-team section-gutter overflow-hidden">
+<div class="container">
+<div class="section-team__accordion accordion accordion-flush" id="accordionResource">
+<ac />
+</div>
+</div>
+</section>
