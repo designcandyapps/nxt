a63d16651ed245c4222fcbed41aaca5d4f92f351
Erin Dunn Halligan <erin@designcandy.com>
2025-11-20 12:25:53 -0600

    Create hd.vue

---
+++ app/components/hd.vue
@@ -0,0 +1,19 @@
+<header class="provider-header">
+  <div class="container">
+    <div class="provider-header--container">
+      <div class="provider-header__menu">
+        <div class="menu-provider-menu-container">
+          <ul class="provider_menu list-unstyled">
+            <li class="menu-item menu-item-has-children"><a>L</a>
+              <ul class="sub-menu">
+                <li class="menu-item"><a>P</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="provider-header__cta-container"><div class="provider-header__cta-wrapper-mobile" data-swap-on-scroll="secondary"></div></div>
+      <div class="provider-menu__toogle"></div>
+    </div>
+  </div>
+</header>
