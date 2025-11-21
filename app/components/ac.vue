ac366c342c8939a3671bb310ba08dbbe1214c331
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 14:02:40 -0600

    Update ac.vue

--- app/components/ac.vue
+++ app/components/ac.vue
@@ -2,13 +2,7 @@
   <div class="accordion-item">
 
     <div class="d-flex align-items-center justify-content-between">
-      <div class="accordion-header pb-5 pb-sm-0 w-100" id="team-heading">
-        <a class="accordion-button p-0 section-team__title text-dark rounded-0 collapsed" data-bs-toggle="collapse" data-bs-target="#team-body" aria-expanded="false" aria-controls="team-body"><h3 class="h3">Meet the team</h3></a></div>
-        <div class="d-none d-sm-block"><a href="#" class="btn btn-secondary btn-icon">Meet team <i class="icon-btn-icon"></i></a></div>
-      </div>
-      <div id="team-body" class="accordion-collapse collapse d-sm-block" aria-labelledby="team-heading" data-bs-parent="#accordionResource">
 
-      </div>
     </div>
 
   </div>
