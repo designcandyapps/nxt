5ba7f2464e13ed1e27036318e735bfeb4bf667de
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 13:28:27 -0600

    Update ac1.vue

--- app/components/ac1.vue
+++ app/components/ac1.vue
@@ -1,11 +1,11 @@
 <div class="swiper-slide pt-8 pt-sm-0">
-<article class="card-team">
-<a href="#"><div class="card-team__img overflow-hidden border-radius-lg ratio ratio-303x379">
-<img decoding="async" class="w-100 object-cover" src="https://zocdocstatic.wpengine.com/wp-content/uploads/2025/04/Nick-Ganju-768x971.jpg">
-</div>
-<div class="card-team__body mt-6 mt-md-8 mt-lg-9"><h5 class="h6 fw-bold">Nick Ganju</h5>
-<p class="body-sm mt-1 text-gray">Founder</p>
-</div>
-</a>
-</article>
+  <article class="card-team">
+  <a href="#">
+    <div class="card-team__img overflow-hidden border-radius-lg ratio ratio-303x379"><img decoding="async" class="w-100 object-cover" src="im/ng.jpg"></div>
+    <div class="card-team__body mt-6 mt-md-8 mt-lg-9">
+      <h5 class="h6 fw-bold">Nick Ganju</h5>
+      <p class="body-sm mt-1 text-gray">Founder</p>
+    </div>
+  </a>
+  </article>
 </div>
