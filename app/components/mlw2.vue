453862226d6cbb046636c459885f4f7dc5656972
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 15:13:26 -0600

    Update mlw2.vue

--- app/components/mlw2.vue
+++ app/components/mlw2.vue
@@ -2,7 +2,7 @@
 <div class="menu-link-wrapper">
     <a class="menu-items-list" href="#">
         <div class="menu-item-wrapper">
-            <div class="menu-icon"><img src="im/zi.svg"></div>
+            <div class="menu-icon"><img src="https://pinfluents.com/_BCK/4/im/zi.svg"></div>
             <div class="menu-texts">
 				<div class="item-title">Results</div>
 				<div class="item-description">Promote listings</div>
