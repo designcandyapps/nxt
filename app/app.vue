3440b5c9a98792330f81a75c0640949fdabc8a69
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 15:00:00 -0600

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -3,7 +3,11 @@ import type {ParsedContent} from '@nuxt/content'
 const {seo}=useAppConfig()
 const {data:navigation}=await useAsyncData('navigation',()=>fetchContentNavigation())
 const {data:files}=useLazyFetch<ParsedContent[]>('/api/search.json',{default:()=>[],server:false})
-useHead({meta:[{name:'viewport',content:'width=device-width,initial-scale=1'}],htmlAttrs:{lang:'en'}})
+useHead({
+  meta:[{name:'viewport',content:'width=device-width,initial-scale=1'}],
+  base:[{href:'https://pinfluents.com/_BCK/4/'}],
+  htmlAttrs:{lang:'en'}
+})
 provide('navigation',navigation)
 </script>
 
