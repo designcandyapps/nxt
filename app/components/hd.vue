92ffd04605212823f1c43fa9ec2ab839164a92b5
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 06:44:51 -0600

    Update hd.vue

--- app/components/hd.vue
+++ app/components/hd.vue
@@ -3,17 +3,24 @@
   <div class="container">
     <div class="provider-header--container">
       <div class="provider-header__menu">
-
         <div class="menu-provider-menu-container">
           <ul class="provider_menu list-unstyled">
             <li class="menu-item menu-item-has-children">
               <a>L</a>
-              <ul class="sub-menu"><li class="menu-item"><a>P</a></li></ul>
+              <ul class="sub-menu">
+                <li class="menu-item"><a>P</a></li>
+                <li class="menu-item"><a>P</a></li>
+                <li class="menu-item"><a>P</a></li>
+              </ul>
             </li>
           </ul>
         </div>
       </div>
-
+      <div class="provider-menu__toogle">
+        <span class="toogle-item"></span>
+        <span class="toogle-item"></span>
+        <span class="toogle-item"></span>
+      </div>
     </div>
   </div>
 </header>
