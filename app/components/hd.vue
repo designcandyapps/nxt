43dfa1abd4b5e0665995ccf539329cbcf0bc59bf
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 08:53:10 -0600

    Update hd.vue

--- app/components/hd.vue
+++ app/components/hd.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import type {NavItem} from '@nuxt/content'
+const navigation=inject<NavItem[]>('navigation',[])
+const {header}=useAppConfig()
+</script>
+
 <template>
   <div class="container">
     <div class="provider-header--container">
