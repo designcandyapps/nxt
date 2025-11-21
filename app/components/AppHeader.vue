791897212ee564b36810347905f394b563cd24a0
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 08:00:22 -0600

    Update AppHeader.vue

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-//import type {NavItem} from '@nuxt/content'
-//const navigation=inject<NavItem[]>('navigation',[])
-//const {header}=useAppConfig()
+import type {NavItem} from '@nuxt/content'
+const navigation=inject<NavItem[]>('navigation',[])
+const {header}=useAppConfig()
 </script>
 
 <template>
-    <img id="ee" src="https://pinfluents.com/_BCK/4/im/bp1.png" width="60" height="60">
+  <UHeader>
+    <template #logo>
+      <template v-if="header?.logo?.dark||header?.logo?.light"><UColorModeImage v-bind="{ class:'h-6 w-auto',...header?.logo }" /></template>
+      <template v-else>
+        <img id="ee" src="https://pinfluents.com/_BCK/4/im/bp1.png" width="60" height="60">
+      </template>
+    </template>
     <hd />
+  </UHeader>
 </template>
