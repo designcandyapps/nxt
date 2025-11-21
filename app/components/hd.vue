f11eeef1bc30014e1355d4ee5f0fe8542bb4eef1
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 05:22:10 -0600

    Update hd.vue

--- app/components/hd.vue
+++ app/components/hd.vue
@@ -6,15 +6,17 @@
         <div class="menu-provider-menu-container">
           <ul class="provider_menu list-unstyled">
             <li class="menu-item menu-item-has-children"><a>L</a>
-              <ul class="sub-menu">
-                <li class="menu-item"><a>P</a></li>
-              </ul>
+              <ul class="sub-menu"><li class="menu-item"><a>P</a></li></ul>
             </li>
           </ul>
         </div>
       </div>
       <div class="provider-header__cta-container"><div class="provider-header__cta-wrapper-mobile" data-swap-on-scroll="secondary"></div></div>
-      <div class="provider-menu__toogle"></div>
+      <div class="provider-menu__toogle">
+        <span class="toogle-item"></span>
+        <span class="toogle-item"></span>
+        <span class="toogle-item"></span>
+      </div>
     </div>
   </div>
 </header>
