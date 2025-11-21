fe597bbc38b710ea194fc1c01cedf7fc09b8ae35
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 13:18:55 -0600

    Create mlw2.vue

---
+++ app/components/mlw2.vue
@@ -0,0 +1,11 @@
+<div class="menu-link-wrapper">
+    <a class="menu-items-list" href="#">
+        <div class="menu-item-wrapper">
+            <div class="menu-icon"><img src="im/zi.svg"></div>
+            <div class="menu-texts">
+		<div class="item-title">Results</div>
+		<div class="item-description">Promote listings</div>
+	    </div>
+        </div>
+    </a>
+</div>
