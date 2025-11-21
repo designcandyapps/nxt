8393b21a20f532837c6e021d621ca9baacb8f8d2
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 14:35:11 -0600

    Update li1.vue

--- app/components/li1.vue
+++ app/components/li1.vue
@@ -1,9 +1,9 @@
 <template>
-        <li>
-          <a href="#">About Us</a>
-          <ul>
-            <li><a href="#">Our Products</a></li>
-            <li><a href="#">Our Services</a></li>
-          </ul>
-        </li>
+  <li>
+    <a href="#">About Us</a>
+      <ul>
+        <li><a href="#">Our Products</a></li>
+        <li><a href="#">Our Services</a></li>
+      </ul>
+  </li>
 </template>
