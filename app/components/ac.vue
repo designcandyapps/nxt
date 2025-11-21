a69f479de16b62899ced47c4f847ca1a6e20134c
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 13:41:54 -0600

    Update ac.vue

--- app/components/ac.vue
+++ app/components/ac.vue
@@ -16,11 +16,20 @@
         <div class="slide-team js-slide-team">
         <div class="swiper-container">
           <div class="swiper-wrapper d-block d-sm-flex">
-            <ac1 />
-            <ac2 />
-            <ac1 />
-            <ac2 />
-            <ac1 />
+
+<div class="swiper-slide pt-8 pt-sm-0">
+  <article class="card-team">
+  <a href="#">
+    <div class="card-team__img overflow-hidden border-radius-lg ratio ratio-303x379"><img decoding="async" class="w-100 object-cover" src="im/ng.jpg"></div>
+    <div class="card-team__body mt-6 mt-md-8 mt-lg-9">
+      <h5 class="h6 fw-bold">Nick Ganju</h5>
+      <p class="body-sm mt-1 text-gray">Founder</p>
+    </div>
+  </a>
+  </article>
+</div>
+
+            
             <ac2 />
           </div>
           <div class="swiper-controls slide-team__controls d-none d-sm-block">
