5fa723905222d9ad56d74e259e5820efcec67688
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 07:24:25 -0600

    Update nav.vue

--- app/components/nav.vue
+++ app/components/nav.vue
@@ -7,14 +7,16 @@
         <div></div>
       </div-->
       <div>
-        <ul>
-          <li><a>Lea</a>
-            <ul>
-              <li><a href="#">Pra</a></li>
-              <li><a href="#">Pro</a></li>
-            </ul>
-          </li>
-        </ul>
+        <div>
+          <ul>
+            <li><a>Lea</a>
+              <ul>
+                <li><a href="#">Pra</a></li>
+                <li><a href="#">Pro</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
       <!--div>
         <span></span>
