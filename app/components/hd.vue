efa7156f439f95207b52a56172c574a7b7df3220
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 08:57:26 -0600

    Update hd.vue

--- app/components/hd.vue
+++ app/components/hd.vue
@@ -1,16 +1,9 @@
-<script setup lang="ts">
-import type {NavItem} from '@nuxt/content'
-const navigation=inject<NavItem[]>('navigation',[])
-const {header}=useAppConfig()
-</script>
-
 <template>
   <div class="container">
     <div class="provider-header--container">
       <div class="provider-header__menu">
-      <div class="menu-provider-menu-container">
-          <UNavigationTree :links="mapContentNavigation(navigation)" />
-          <!--ul class="provider_menu list-unstyled">
+        <div class="menu-provider-menu-container">
+          <ul class="provider_menu list-unstyled">
             <li class="menu-item menu-item-has-children">
               <a>L</a>
               <ul class="sub-menu">
@@ -19,7 +12,7 @@ const {header}=useAppConfig()
                 <li class="menu-item"><a>P</a></li>
               </ul>
             </li>
-          </ul-->
+          </ul>
       </div>
       </div>
       <!--div class="provider-menu__toogle">
