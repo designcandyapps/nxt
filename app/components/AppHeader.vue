50b0ff4130d9ae146212675efe224c8feaf8b405
Erin Dunn Halligan <erin@designcandy.com>
2025-11-21 08:43:18 -0600

    Update AppHeader.vue

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -9,7 +9,7 @@ const {header}=useAppConfig()
     <template #logo>
       <template v-if="header?.logo?.dark||header?.logo?.light"><UColorModeImage v-bind="{ class:'h-6 w-auto',...header?.logo }" /></template>
       <template v-else>
-        <img id="ee" src="https://pinfluents.com/_BCK/4/im/bp.png" width="60" height="60">
+        <!--img id="ee" src="https://pinfluents.com/_BCK/4/im/bp.png" width="60" height="60"-->
       </template>
     </template>
     <hd />
