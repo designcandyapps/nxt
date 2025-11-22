60d9fea70e1cd0a8aa7a72638e522dca05c930e5
Erin Dunn Halligan <erin@designcandy.com>
2025-11-22 10:34:17 -0600

    Update nf.vue

--- app/components/nf.vue
+++ app/components/nf.vue
@@ -1,3 +1,4 @@
+<template>
 <div class="flex justify-between select-none items-center rounded-md px-2.5 py-1.5 gap-2 relative cursor-pointer" id="headlessui-combobox-option-v-0-0-0-16" role="option" aria-selected="false" data-headlessui-state="">
 
   <div class="flex items-center gap-1.5 min-w-0">
@@ -10,4 +11,6 @@
     <kbd class="inline-flex items-center justify-center text-gray-900 dark:text-white h-5 min-w-[20px] text-[11px] px-1 rounded font-medium font-sans bg-gray-100 dark:bg-gray-800 ring-1 ring-gray-300 dark:ring-gray-700 ring-inset">N</kbd>
   </span>
 
+
 </div>
+</template>
