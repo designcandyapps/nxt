0fd5fb99dddca27b15152ada08b9dfc0cd48178e
Erin Dunn Halligan <erin@designcandy.com>
2025-11-22 09:59:59 -0600

    Add files via upload

--- app/components/not1.vue
+++ app/components/not1.vue
@@ -3,9 +3,9 @@
   <div class="flex p-4 gap-3 items-start">
     <span class="iconify i-heroicons:command-line flex-shrink-0 w-5 h-5 text-primary-500 dark:text-primary-400" aria-hidden="true"></span>
     <div class="w-0 flex-1">
-    <p class="text-sm font-medium text-gray-900 dark:text-white">Notification</p>
-    <div class="mt-1 text-sm leading-4 text-gray-500 dark:text-gray-400">This is a notification!</div>
-  </div>
+      <p class="text-sm font-medium text-gray-900 dark:text-white">Notification</p>
+      <div class="mt-1 text-sm leading-4 text-gray-500 dark:text-gray-400">This is a notification!</div>
+    </div>
   </div>
   <div class="absolute bottom-0 end-0 start-0 h-1 bg-primary-500 dark:bg-primary-400" style="width: 0%;"></div>
 
