dfff0cba459c500163fcbad8123322ac5332890d
Erin Dunn Halligan <erin@designcandy.com>
2025-11-22 10:15:35 -0600

    Update New.vue

--- app/components/New.vue
+++ app/components/New.vue
@@ -2,26 +2,16 @@
 <div class="flex flex-col lg:grid gap-8 relative lg:grid-cols-10 lg:gap-8 overflow-hidden p-1">
 
   <div class="col-span-8 flex items-center animate-top">
-
     <div class="relative w-full flex items-center h-4">
       <inp1 />
     </div>
-
   </div>
 
   <div class="col-span-2 row-span-2 flex items-center animate-right">
   <div class="space-y-1">
-
     <div class="relative flex items-start">
       <inp2 />
     </div>
-    <div class="relative flex items-start">
-      <inp3 />
-    </div>
-    <div class="relative flex items-start">
-      <inp4 />
-    </div>
-
   </div>
   </div>
 
