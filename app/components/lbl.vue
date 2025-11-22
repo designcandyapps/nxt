99a5d155b2130bf421ef0a774506fd4307d4e371
Erin Dunn Halligan <erin@designcandy.com>
2025-11-22 10:34:11 -0600

    Update lbl.vue

--- app/components/lbl.vue
+++ app/components/lbl.vue
@@ -1,3 +1,4 @@
+<template>
 <div class="flex justify-between select-none items-center rounded-md px-2.5 py-1.5 gap-2 relative cursor-pointer" id="headlessui-combobox-option-v-0-0-0-19" role="option" aria-selected="false" data-headlessui-state="">
 
   <div class="flex items-center gap-1.5 min-w-0">
@@ -6,4 +7,6 @@
   </div>
   <span class="hidden md:inline-flex flex-shrink-0 gap-0.5"><kbd class="inline-flex items-center justify-center text-gray-900 dark:text-white h-5 min-w-[20px] text-[11px] px-1 rounded font-medium font-sans bg-gray-100 dark:bg-gray-800 ring-1 ring-gray-300 dark:ring-gray-700 ring-inset">⌘</kbd><kbd class="inline-flex items-center justify-center text-gray-900 dark:text-white h-5 min-w-[20px] text-[11px] px-1 rounded font-medium font-sans bg-gray-100 dark:bg-gray-800 ring-1 ring-gray-300 dark:ring-gray-700 ring-inset">L</kbd></span>
 
+
 </div>
+</template>
