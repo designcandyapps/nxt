3cbc6d20bf18b57f79191394178f5f7a63660f7b
Erin Dunn Halligan <erin@designcandy.com>
2025-11-22 09:44:07 -0600

    Add files via upload

---
+++ app/components/btn99.vue
@@ -0,0 +1,7 @@
+<template>
+<div id="headlessui-combobox-button-v-0-0-0-22" aria-haspopup="listbox" aria-expanded="false" data-headlessui-state="" role="button" class="flex items-center w-full">
+
+  <button id="v-0-0-0-21" class="relative w-full disabled:cursor-not-allowed disabled:opacity-75 focus:outline-none border-0 inline-flex items-center text-left cursor-default rounded-md text-sm gap-x-1.5 px-2.5 py-1.5 shadow-sm bg-white dark:bg-gray-900 text-gray-900 dark:text-white ring-1 ring-inset ring-gray-300 dark:ring-gray-700 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-400 pe-9" type="button"><span class="text-gray-500 dark:text-gray-400 truncate">Select labels</span><span class="absolute inset-y-0 end-0 flex items-center pointer-events-none px-2.5"><span class="iconify i-heroicons:chevron-down-20-solid flex-shrink-0 text-gray-400 dark:text-gray-500 h-5 w-5" aria-hidden="true"></span></span></button>
+
+</div>
+</template>
