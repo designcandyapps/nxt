55fcbb21ba8376fb69c4f04d987d1f4fe0bf71f6
Erin Dunn Halligan <erin@designcandy.com>
2025-11-24 09:22:10 -0600

    Update New.vue

--- app/components/New.vue
+++ app/components/New.vue
@@ -1,17 +1,23 @@
 <template>
 <div class="flex flex-col lg:grid gap-8 relative lg:grid-cols-10 lg:gap-8 overflow-hidden p-1">
 
-<not1 />
+<!--not1 />
 <fm1 /><fm2 />
-
 <btns1 />
 <pul />
-
 <not />
 <se />
 <btn99 />
+<tbl /-->
 
-<tbl />
+<btns />
+<h />
+<inp1 /><inp2 /><inp3 /><inp4 /><inp5 />
+<kbd />
+<lbl />
+<nf />
+<p1 />
+<spn />
 
 </div>
 </template>
