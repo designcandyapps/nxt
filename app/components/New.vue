77b582052cf365e6f655ebc3a0770b678288ac3f
Erin Dunn Halligan <erin@designcandy.com>
2025-11-24 08:19:31 -0600

    Update New.vue

--- app/components/New.vue
+++ app/components/New.vue
@@ -20,66 +20,6 @@
   <pul />
 </div>
 
-<div class="col-span-5 row-span-2 flex flex-col animate-bottom">
-<div class="rounded-lg divide-y divide-gray-200 dark:divide-gray-800 ring-1 ring-gray-200 dark:ring-gray-800 shadow bg-white dark:bg-gray-900 col-span-4 row-span-6 flex-1 flex flex-col">
-<div class="flex-1 flex flex-col overflow-y-auto">
-<div class="flex flex-col flex-1 min-h-0 divide-y divide-gray-100 dark:divide-gray-800" data-headlessui-state="">
-  <div class="relative w-full flex items-center h-4">
-    <inp1 />
-  </div>
-  <div class="relative flex items-start">
-    <inp2 />
-  </div>
-  <div class="relative flex items-start">
-    <inp3 />
-  </div>
-  <div class="relative flex items-start">
-    <inp4 />
-  </div>
-    <div class="relative flex items-start">
-    <inp4 />
-  </div>
-
-
-  
-  <div id="headlessui-combobox-options-v-0-0-0-14" role="listbox" data-headlessui-state="" aria-label="Commands" class="relative flex-1 overflow-y-auto divide-y divide-gray-100 dark:divide-gray-800 scroll-py-2">
-    <div class="p-2">
-      <p1 />
-    </div>
-    <div class="p-2">
-      <div class="text-sm text-gray-700 dark:text-gray-200">
-        <nf />
-        <kbd />
-        <h />
-        <lbl />
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-</div>
-</div>
-
-<div class="col-span-2 row-span-2 gap-6 flex flex-col animate-right z-10">
-  <div class="relative flex items-start">
-    <not />
-  </div>
-  <div class="relative">
-    <se />
-  </div>
-  <div>
-    <div><div class="flex content-center items-center justify-between text-sm"><label for="v-0-0-0-21" class="block font-medium text-gray-700 dark:text-gray-200">Labels</label></div></div>
-    <div class="mt-1 relative"><div class="relative" data-headlessui-state="">
-    <btn99 />
-    </div></div>
-  </div>
-  <div class="rounded-lg divide-y divide-gray-200 dark:divide-gray-800 ring-1 ring-gray-200 dark:ring-gray-800 shadow bg-white dark:bg-gray-900">
-    <div class="px-4 py-5 sm:p-6 !p-1">
-      <nav />
-    </div>
-  </div>
-</div>
-
 <div class="col-span-7 row-span-6 animate-bottom">
   <div class="rounded-lg divide-y divide-gray-200 dark:divide-gray-800 ring-1 ring-gray-200 dark:ring-gray-800 shadow bg-white dark:bg-gray-900">
     <div>
