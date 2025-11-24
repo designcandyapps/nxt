23825294bb1e4bcd7b967d952744d9c743174c91
Erin Dunn Halligan <erin@designcandy.com>
2025-11-24 08:54:19 -0600

    Update New.vue

--- app/components/New.vue
+++ app/components/New.vue
@@ -14,5 +14,37 @@
   <pul />
 </div>
 
+
+
+<div class="col-span-2 row-span-2 gap-6 flex flex-col animate-right z-10">
+  <div class="relative flex items-start">
+    <not />
+  </div>
+  <div class="relative">
+    <se />
+  </div>
+  <div>
+    <div><div class="flex content-center items-center justify-between text-sm"><label for="v-0-0-0-21" class="block font-medium text-gray-700 dark:text-gray-200">Labels</label></div></div>
+    <div class="mt-1 relative"><div class="relative" data-headlessui-state="">
+    <btn99 />
+    </div></div>
+  </div>
+  <div class="rounded-lg divide-y divide-gray-200 dark:divide-gray-800 ring-1 ring-gray-200 dark:ring-gray-800 shadow bg-white dark:bg-gray-900">
+    <div class="px-4 py-5 sm:p-6 !p-1">
+      <nav />
+    </div>
+  </div>
+</div>
+
+<div class="col-span-7 row-span-6 animate-bottom">
+  <div class="rounded-lg divide-y divide-gray-200 dark:divide-gray-800 ring-1 ring-gray-200 dark:ring-gray-800 shadow bg-white dark:bg-gray-900">
+    <div>
+      <div class="relative overflow-x-auto">
+        <tbl />
+      </div>
+    </div>
+  </div>
+</div>
+  
 </div>
 </template>
