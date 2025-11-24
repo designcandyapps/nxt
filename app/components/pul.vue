3c6dcc8827b819343db3a0894ee2434a05216676
Erin Dunn Halligan <erin@designcandy.com>
2025-11-24 08:49:04 -0600

    Update pul.vue

--- app/components/pul.vue
+++ app/components/pul.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="rounded-lg divide-y divide-gray-200 dark:divide-gray-800 ring-1 ring-gray-200 dark:ring-gray-800 shadow bg-white dark:bg-gray-900 flex-shrink-0">
+
 <div class="px-4 py-5 sm:p-6">
 <div class="flex items-center gap-4 justify-center">
 
