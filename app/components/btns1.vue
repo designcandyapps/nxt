047583931b19e8c2fa8f94f5375e2b872615bc83
Erin Dunn Halligan <erin@designcandy.com>
2025-11-24 08:42:17 -0600

    Update btns1.vue

--- app/components/btns1.vue
+++ app/components/btns1.vue
@@ -1,12 +1,12 @@
 <template>
-<div role="tablist" aria-orientation="horizontal" class="relative bg-gray-100 dark:bg-gray-800 rounded-lg p-1 w-full h-10 inline-grid items-center" style="grid-template-columns:repeat(2,minmax(0px,1fr));">
-  <div class="absolute top-[4px] left-[4px] duration-200 ease-out focus:outline-none" style="top: 4px; left: 4px; width: 190px; height: 32px;">
+<div class="relative bg-gray-100 dark:bg-gray-800 rounded-lg p-1 w-full h-10 inline-grid items-center" style="grid-template-columns:repeat(2,minmax(0px,1fr));">
+
+  <div class="absolute top-[4px] left-[4px] duration-200 ease-out focus:outline-none" style="top:4px; left:4px; width:190px; height:32px;">
     <div class="w-full h-full bg-white dark:bg-gray-900 rounded-md shadow-sm"></div>
   </div>
 
-  <button class="relative inline-flex items-center justify-center flex-shrink-0 w-full ui-focus-visible:outline-0 ui-focus-visible:ring-2 ui-focus-visible:ring-primary-500 dark:ui-focus-visible:ring-primary-400 ui-not-focus-visible:outline-none focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 transition-colors duration-200 ease-out h-8 px-3 text-sm font-medium rounded-md text-gray-900 dark:text-white" id="headlessui-tabs-tab-v-0-0-0-5" role="tab" aria-selected="true" data-headlessui-state="selected" type="button" aria-controls="headlessui-tabs-panel-v-0-0-0-7"><span class="truncate">Account</span></button>
-
-  <button class="relative inline-flex items-center justify-center flex-shrink-0 w-full ui-focus-visible:outline-0 ui-focus-visible:ring-2 ui-focus-visible:ring-primary-500 dark:ui-focus-visible:ring-primary-400 ui-not-focus-visible:outline-none focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 transition-colors duration-200 ease-out h-8 px-3 text-sm font-medium rounded-md text-gray-500 dark:text-gray-400" id="headlessui-tabs-tab-v-0-0-0-6" role="tab" aria-selected="false" data-headlessui-state="" type="button" aria-controls="headlessui-tabs-panel-v-0-0-0-10"><span class="truncate">Password</span></button>
+  <button type="button" class="relative inline-flex items-center justify-center flex-shrink-0 w-full ui-focus-visible:outline-0 ui-focus-visible:ring-2 ui-focus-visible:ring-primary-500 dark:ui-focus-visible:ring-primary-400 ui-not-focus-visible:outline-none focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 transition-colors duration-200 ease-out h-8 px-3 text-sm font-medium rounded-md text-gray-900 dark:text-white" data-headlessui-state="selected"><span class="truncate">Account</span></button>
+  <button type="button" class="relative inline-flex items-center justify-center flex-shrink-0 w-full ui-focus-visible:outline-0 ui-focus-visible:ring-2 ui-focus-visible:ring-primary-500 dark:ui-focus-visible:ring-primary-400 ui-not-focus-visible:outline-none focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 transition-colors duration-200 ease-out h-8 px-3 text-sm font-medium rounded-md text-gray-500 dark:text-gray-400"><span class="truncate">Password</span></button>
 
 </div>
 </template>
