5ead5666644f65838b840c4780c101b36a293526
Erin Dunn Halligan <erin@designcandy.com>
2025-12-01 02:12:17 -0600

    Update Card.vue

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -1,14 +1,36 @@
 <template>
-  <header style="border:3px dashed red;">
-  <div>
-    <div>
-      <ul>
-        <li2 />
-        <li2 />
-        <li2 />
-        <li2 />
-      </ul>
+<header class="provider-header">
+  <div class="container">
+    <div class="provider-header--container">
+
+      <div class="provider-header__cta-container">
+	<a class="provider-image-link provider-image-link-mobile" href="#" data-swap-on-scroll="primary" style="opacity:0; visibility:hidden;"><img id="ee" src="https://pinfluents.com/_BCK/4/im/zo.png"></a>
+        <div class="provider-header__cta-wrapper-mobile" data-swap-on-scroll="secondary" style="opacity:1; visibility:visible; -border:3px dashed blue;">
+	  <a class="provider-image-link provider-image-link-mobile-icon" href="#"><img id="ee" src="https://pinfluents.com/_BCK/4/im/zo.png"></a>
+        </div>
+      </div>
+
+      <div class="provider-header__menu">
+        <a class="provider-image-link provider-image-link-desktop" href="#"><img id="ee" src="im/zo.png"></a>
+        <div class="menu-provider-menu-container">
+          <ul class="provider_menu list-unstyled">
+            <li class="menu-item menu-item-has-children"><a>Learn</a>
+              <ul class="sub-menu">
+                <li class="menu-item"><a href="#">Practice</a></li>
+                <li class="menu-item"><a href="#">Product</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="provider-menu__toogle">
+        <span class="toogle-item"></span>
+        <span class="toogle-item"></span>
+        <span class="toogle-item"></span>
+      </div>
+
     </div>
   </div>
-  </header>
+</header>
 </template>
