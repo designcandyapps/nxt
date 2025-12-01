cfc97735d827408e9a37f92020becc8cb1122fd3
Erin Dunn Halligan <erin@designcandy.com>
2025-12-01 04:11:39 -0600

    Update Card.vue

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -1,13 +1,11 @@
 <template>
-<header class="provider-header">
-  <div class="container">
-    <div class="provider-header--container open">
-      <div class="provider-header__menu">
-	      <ul class="provider_menu list-unstyled">
-	        <li class="menu-item menu-item-has-children">Learn<div class="sub-menu" style="display:none;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris nisi ut aliquip ex ea commodo consequat.</div></li>
+  <div>
+    <div>
+      <div>
+	      <ul>
+	        <li>Learn<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></li>
 	      </ul>
       </div>
     </div>
   </div>
-</header>
 </template>
