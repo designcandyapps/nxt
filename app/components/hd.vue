95a3b87b2bf62921b588a7ea6fb36dfc6a174e56
Erin Dunn Halligan <erin@designcandy.com>
2025-12-01 11:00:38 -0600

    Update hd.vue

--- app/components/hd.vue
+++ app/components/hd.vue
@@ -1,12 +1,9 @@
 <template>
-<header>
-  <div><div>
-    <ul>
-      <li1 />
-      <li1 />
-      <li1 />
-      <li1 />
-    </ul>
-  </div></div>
-</header>
+  <ul>
+    <li1 />
+    <li1 />
+    <li1 />
+    <li1 />
+  </ul>
 </template>
+
