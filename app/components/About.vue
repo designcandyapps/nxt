14f964162da66d989f5ae7c2506558dd078f87a5
Erin Dunn Halligan <erin@designcandy.com>
2025-12-01 15:12:06 -0600

    Update About.vue

--- app/components/About.vue
+++ app/components/About.vue
@@ -1,5 +1,9 @@
 <template>
 <section class="section-team section-gutter overflow-hidden">
-  <ac />
+  <div class="container">
+    <div class="section-team__accordion accordion accordion-flush" id="accordionResource">
+      <ac />
+    </div>
+  </div>
 </section>
 </template>
