9c89469fd358fc8adc9fa627d607c822386b8545
Erin Dunn Halligan <erin@designcandy.com>
2025-12-01 08:11:45 -0600

    Refactor Card component layout structure

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -1,7 +1,11 @@
 <template>
 <div>
-  <ul>
-	<li>Learn<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris nisi ut aliquip commodo consequat.</div></li>
-  </ul>
+  <div>
+    <div>
+	  <ul>
+	    <li>Learn<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris nisi ut aliquip commodo consequat.</div></li>
+	  </ul>
+    </div>
+  </div>
 </div>
 </template>
