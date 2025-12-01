425faede671b5b99053bfaa7633fb5fb331c621d
Erin Dunn Halligan <erin@designcandy.com>
2025-12-01 15:04:26 -0600

    Update Stripe.vue

--- app/components/Stripe.vue
+++ app/components/Stripe.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
-    <form id="payment-form">
-      <div id="link-authentication-element"><!--Authentication element--></div>
-      <div id="payment-element"></div>
-      <button id="submit">Pay now</button>
-      <div id="error-message"></div>
-    </form>
-    <div id="messages" role="alert" style="display:none;"></div>
-  </div>
+  <form id="payment-form">
+    <div id="link-authentication-element"><!--Authentication element--></div>
+    <div id="payment-element"></div>
+    <button id="submit">Pay now</button>
+    <div id="error-message"></div>
+  </form>
+  <div id="messages" role="alert" style="display:none;"></div>
 </template>
