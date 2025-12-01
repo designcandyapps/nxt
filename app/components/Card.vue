0ada78f55969a0f39ede61c8429bab8869e35259
Erin Dunn Halligan <erin@designcandy.com>
2025-12-01 03:38:02 -0600

    Fix sub-menu display style in Card.vue

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -4,7 +4,7 @@
     <div class="provider-header--container open">
       <div class="provider-header__menu">
 	      <ul class="provider_menu list-unstyled">
-	        <li class="menu-item menu-item-has-children">Learn<div class="sub-menu" style="display: none;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div></li>
+	        <li class="menu-item menu-item-has-children">Learn<div class="sub-menu" style="display:none;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris nisi ut aliquip ex ea commodo consequat.</div></li>
 	      </ul>
       </div>
     </div>
