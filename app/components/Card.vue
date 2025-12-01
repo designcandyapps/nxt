a30f09fe426b76f371dbf8ded979b43d9125587f
Erin Dunn Halligan <erin@designcandy.com>
2025-12-01 04:46:59 -0600

    Refactor provider menu structure in Card.vue

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -2,11 +2,9 @@
 <div>
   <div class="container">
     <div class="provider-header--container open">
-
-	      <ul class="provider_menu list-unstyled">
-	        <li class="menu-item menu-item-has-children">Learn<p class="sub-menu" style="display:none;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></li>
-	      </ul>
-
+	  <ul class="provider_menu list-unstyled">
+	    <li class="menu-item menu-item-has-children">Learn<p class="sub-menu">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></li>
+	  </ul>
     </div>
   </div>
 </div>
