ce91e60fd5aafa18d24bafaf7e174af7fefd4351
Erin Dunn Halligan <erin@designcandy.com>
2025-12-01 04:33:49 -0600

    Refactor Card.vue header structure and classes

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -1,10 +1,10 @@
 <template>
-<header>
-  <div>
-    <div>
-      <div>
-	      <ul>
-	        <li>Learn<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></li>
+<header class="provider-header">
+  <div class="container">
+    <div class="provider-header--container open">
+      <div class="provider-header__menu">
+	      <ul class="provider_menu list-unstyled">
+	        <li class="menu-item menu-item-has-children">Learn<p class="sub-menu" style="display:none;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></li>
 	      </ul>
       </div>
     </div>
