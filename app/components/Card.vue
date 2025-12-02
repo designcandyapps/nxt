51c3b0fddfce3cb6826a81d61b733d82596b49d5
Erin Dunn Halligan <erin@designcandy.com>
2025-12-02 11:37:02 -0600

    Update Card.vue

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="dv">
     <h2>Product</h2>
     <img src="https://designcandy.com/im/dc.png">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris nisi ut aliquip commodo consequat.
