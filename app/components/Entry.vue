94543968885672779f445beeab0cf40a68b207f5
Erin Dunn Halligan <erin@designcandy.com>
2025-12-02 13:32:59 -0600

    Update Entry.vue

--- app/components/Entry.vue
+++ app/components/Entry.vue
@@ -3,8 +3,7 @@
   <div id="auth"><!--Login--></div>
   <div id="login"></div>
 
-  <inp1 /><inp2 />
-  <tabs />
+  <inp1 /><inp3 />
 
   <button id="submit">Order</button>
   <div id="error-message"></div>
