ae1ad2738d374e64959858b977f5dbe1cdc8192e
Erin Dunn Halligan <erin@designcandy.com>
2025-12-02 12:14:47 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -44,7 +44,7 @@ onMounted(()=>{
   <ULandingSection :title="page.sections.title" :links="page.sections.links"><UPageGrid><ULandingCard v-for="(item,index) of page.sections.items" :key="index" v-bind="item" /><Slider /></UPageGrid></ULandingSection>
   <ULandingSection :title="page.mid.title" :links="page.mid.links"><UPageGrid><ULandingCard v-for="(item,index) of page.mid.items" :key="index" v-bind="item" /><Entry /></UPageGrid></ULandingSection>
   <ULandingSection :title="page.bottom.title" :links="page.bottom.links"><UPageGrid><ULandingCard v-for="(item,index) of page.bottom.items" :key="index" v-bind="item" /><Stripe /></UPageGrid></ULandingSection>
-  <ULandingSection :title="page.lower.title" :links="page.lower.links"><UPageGrid><ULandingCard v-for="(item,index) of page.lower.items" :key="index" v-bind="item" /><Acc /></UPageGrid></ULandingSection>
+  <ULandingSection :title="page.lower.title" :links="page.lower.links"><UPageGrid><ULandingCard v-for="(item,index) of page.lower.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
 </template>
 
 <script lang="ts">
