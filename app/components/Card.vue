2d90f2428812cc54db20ca531f2375a68751e0fa
Erin Dunn Halligan <erin@designcandy.com>
2025-12-02 07:44:11 -0600

    Create Card.vue

---
+++ app/components/Card.vue
@@ -0,0 +1,5 @@
+<template>
+	<h2>Product</h2>
+    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris nisi ut aliquip commodo consequat.</div></li>
+  </h2
+</template>
