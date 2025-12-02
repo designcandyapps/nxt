d9a3eae1c7b25db8dbfbb5dbb577273050e09af8
Erin Dunn Halligan <erin@designcandy.com>
2025-12-02 10:46:03 -0600

    Update Stripe.vue

--- app/components/Stripe.vue
+++ app/components/Stripe.vue
@@ -3,7 +3,7 @@
     <Cart />
     <div id="link-authentication-element"><!--Authentication element--></div>
     <div id="payment-element"></div>
-    <button id="submit">Pay now</button>
+    <button id="submit">Pay</button>
     <div id="error-message"></div>
   </form>
   <div id="messages" role="alert" style="display:none;"></div>
