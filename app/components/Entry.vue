35c9aa4f4fd60f60121d8b6f503f878f6e5e7283
Erin Dunn Halligan <erin@designcandy.com>
2025-12-02 08:05:03 -0600

    Create Entry.vue

---
+++ app/components/Entry.vue
@@ -0,0 +1,9 @@
+<template>
+  <form id="entry">
+    <div id="link-authentication-element"><!--Login element--></div>
+    <div id="login-element"></div>
+    <button id="submit">Order now</button>
+    <div id="error-message"></div>
+  </form>
+  <div id="messages" role="alert" style="display:none;"></div>
+</template>
