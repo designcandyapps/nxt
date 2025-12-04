723392277fb32f28b1b5b1900927b72578bda24a
Erin Dunn Halligan <erin@designcandy.com>
2025-12-04 14:01:01 -0600

    Update AppHeader.vue

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,46 +1,9 @@
 <script setup lang="ts">
-import type { NavItem } from '@nuxt/content'
-const navigation = inject<NavItem[]>('navigation', [])
-const { header } = useAppConfig()
+import type {NavItem} from '@nuxt/content'
+const navigation=inject<NavItem[]>('navigation',[])
+const {header}=useAppConfig()
 </script>
 
 <template>
-  <UHeader>
-    <template #logo>
-      <template v-if="header?.logo?.dark || header?.logo?.light">
-        <UColorModeImage v-bind="{ class: 'h-6 w-auto', ...header?.logo }" />
-      </template>
-      <template v-else>
-        <img id="ee" src="https://pinfluents.com/_BCK/4/im/dc.png" width="60" height="60">
-      </template>
-    </template>
-
-    <template
-      v-if="header?.search"
-      #center
-    >
-      <UContentSearchButton class="hidden lg:flex" />
-    </template>
-
-    <template #right>
-      <UContentSearchButton
-        v-if="header?.search"
-        :label="null"
-        class="lg:hidden"
-      />
-      <UColorModeButton v-if="header?.colorMode" />
-
-      <template v-if="header?.links">
-        <UButton
-          v-for="(link, index) of header.links"
-          :key="index"
-          v-bind="{ color: 'gray', variant: 'ghost', ...link }"
-        />
-      </template>
-    </template>
-
-    <template #panel>
-      <UNavigationTree :links="mapContentNavigation(navigation)" />
-    </template>
-  </UHeader>
+  <hd />
 </template>
