f2953f39c24a19534195a5a3105d7fc332058af7
Erin Dunn Halligan <erin@designcandy.com>
2025-12-04 14:40:13 -0600

    Update AppHeader.vue

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import type { NavItem } from '@nuxt/content'
-const navigation = inject<NavItem[]>('navigation', [])
-const { header } = useAppConfig()
+import type {NavItem} from '@nuxt/content'
+const navigation=inject<NavItem[]>('navigation',[])
+const {header}=useAppConfig()
 </script>
 
 <template>
