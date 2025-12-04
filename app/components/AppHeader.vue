634eb5cd3592642ca226cc7814b91030a608bcd5
Erin Dunn Halligan <erin@designcandy.com>
2025-12-04 14:28:12 -0600

    Update AppHeader.vue

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -15,30 +15,6 @@ const { header } = useAppConfig()
       </template>
     </template>
 
-    <template
-      v-if="header?.search"
-      #center
-    >
-      <UContentSearchButton class="hidden lg:flex" />
-    </template>
-
-    <template #right>
-      <UContentSearchButton
-        v-if="header?.search"
-        :label="null"
-        class="lg:hidden"
-      />
-      <UColorModeButton v-if="header?.colorMode" />
-
-      <template v-if="header?.links">
-        <UButton
-          v-for="(link, index) of header.links"
-          :key="index"
-          v-bind="{ color: 'gray', variant: 'ghost', ...link }"
-        />
-      </template>
-    </template>
-
     <template #panel>
       <UNavigationTree :links="mapContentNavigation(navigation)" />
     </template>
