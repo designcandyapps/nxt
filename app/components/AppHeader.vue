68664abf1232f6e9c1d1b1bdd626d261166bde6a
Erin Dunn Halligan <erin@designcandy.com>
2025-12-06 05:50:26 -0600

    Update AppHeader.vue

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -6,7 +6,23 @@ const {header}=useAppConfig()
 
 <template>
   <UHeader>
-    <img id="ee" src="https://pinfluents.com/_BCK/4/im/dc.png" width="60" height="60">
-    <hd />
+    <template #logo>
+      <template v-if="header?.logo?.dark||header?.logo?.light"><UColorModeImage v-bind="{ class:'h-6 w-auto',...header?.logo }" /></template>
+      <template v-else>
+        <img id="ee" src="https://pinfluents.com/_BCK/4/im/bp1.png" width="60" height="60">
+        <!--NuxtImg id="nu" src="/public/dc2.png" width="60" height="60" format="png" @load="getCP" /-->
+      </template>
+    </template>
+
+    <template v-if="header?.search" #center>
+      <UContentSearchButton class="hidden lg:flex" />
+    </template>
+
+    <template #right>
+      <UContentSearchButton v-if="header?.search" :label="null" class="lg:hidden" />
+      <template v-if="header?.links"><UButton v-for="(link,index) of header.links" :key="index" v-bind="{ color:'gray',variant:'ghost',...link }" /></template>
+    </template>
+
+    <template #panel><UNavigationTree :links="mapContentNavigation(navigation)" /></template>
   </UHeader>
 </template>
