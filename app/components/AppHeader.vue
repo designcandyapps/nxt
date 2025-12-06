3f36b3e171dc5ddbab20eb50f70ae6f14409af4f
Erin Dunn Halligan <erin@designcandy.com>
2025-12-06 07:35:01 -0600

    Update AppHeader.vue

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -6,6 +6,7 @@ const {header}=useAppConfig()
 
 <template>
   <UHeader>
+    <img id="ee" src="https://pinfluents.com/_BCK/4/im/bp1.png" width="60" height="60">
     <hd />
   </UHeader>
 </template>
