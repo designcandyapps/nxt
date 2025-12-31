137983c9eb1e75b907811768456bf2bdb5de27d8
Erin Dunn Halligan <erin@designcandy.com>
2025-12-31 06:29:29 -0600

    Update Slider.vue

--- app/components/Slider.vue
+++ app/components/Slider.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="lp-block lp-content lp-wrapper lp-wrapper--layout-wide lp-hosting-content lp-grid lp-grid--columns-1@XS lp-grid--columns-2@S lp-grid--columns-4@L lp-grid--gutter-3 lp-pt-30@S news-slider slick-initialized slick-slider slick-dotted">
+  <div class="news-slider slick-initialized slick-slider slick-dotted">
 
 <div>
 <p>
-<section class="lp-block lp-card lp-pb-18">
-<div class="lp-block lp-icons lp-icons--align-left lp-icons--size-36 lp-margin-bottom-3 is-style-icons-bullets" aria-hidden="true">
-<div class="lp-icons__content" style="margin: 0px; color: rgb(203, 213, 225);"><div class="lp-block lp-icon-svg" data-name="@wordpress/icons/trendingUp">
-<div class="lp-icon-svg__content"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M3.445 16.505a.75.75 0 001.06.05l5.005-4.55 4.024 3.521 4.716-4.715V14h1.5V8.25H14v1.5h3.19l-3.724 3.723L9.49 9.995l-5.995 5.45a.75.75 0 00-.05 1.06z"></path></svg></div></div></div></div>
-<div class="lp-block lp-content lp-content--text-alignment lp-content--text-alignment-left">
-<h2 class="wp-block-heading lp-text-32 is-style-heading-body has-normal-font-size">Room to grow</h2>
+<section>
+<div>
+<div>
+<div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3.445 16.505a.75.75 0 001.06.05l5.005-4.55 4.024 3.521 4.716-4.715V14h1.5V8.25H14v1.5h3.19l-3.724 3.723L9.49 9.995l-5.995 5.45a.75.75 0 00-.05 1.06z"></path></svg></div></div></div></div>
+<div>
+<h2>Room to grow</h2>
 <p>Scale as you go with unmetered bandwidth on all plans. Zero downtime. No limits.</p>
 </div>
 </section>
@@ -17,12 +17,12 @@
 
 <div>
 <p>
-<section class="lp-block lp-card lp-pb-18">
-<div class="lp-block lp-icons lp-icons--align-left lp-icons--size-36 lp-margin-bottom-3 is-style-icons-bullets" aria-hidden="true">
-<div class="lp-icons__content" style="margin: 0px; color: rgb(203, 213, 225);"><div class="lp-block lp-icon-svg" data-name="@wordpress/icons/trendingUp">
-<div class="lp-icon-svg__content"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M3.445 16.505a.75.75 0 001.06.05l5.005-4.55 4.024 3.521 4.716-4.715V14h1.5V8.25H14v1.5h3.19l-3.724 3.723L9.49 9.995l-5.995 5.45a.75.75 0 00-.05 1.06z"></path></svg></div></div></div></div>
-<div class="lp-block lp-content lp-content--text-alignment lp-content--text-alignment-left">
-<h2 class="wp-block-heading lp-text-32 is-style-heading-body has-normal-font-size">Room to grow</h2>
+<section>
+<div>
+<div>
+<div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3.445 16.505a.75.75 0 001.06.05l5.005-4.55 4.024 3.521 4.716-4.715V14h1.5V8.25H14v1.5h3.19l-3.724 3.723L9.49 9.995l-5.995 5.45a.75.75 0 00-.05 1.06z"></path></svg></div></div></div></div>
+<div>
+<h2>Room to grow</h2>
 <p>Scale as you go with unmetered bandwidth on all plans. Zero downtime. No limits.</p>
 </div>
 </section>
