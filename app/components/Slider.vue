2848708cc2bc30c5e1b31902e98b19dfe303db5e
Erin Dunn Halligan <erin@designcandy.com>
2025-12-31 12:45:12 -0600

    Update Slider.vue

--- app/components/Slider.vue
+++ app/components/Slider.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="news-slider">
-    <div>Ee</div>
-    <div>Ff</div>
-    <div>Gg</div>
+      <section class="lp-block lp-card lp-pb-18">
+        <div class="lp-block lp-content lp-content--text-alignment lp-content--text-alignment-left"><h2 class="wp-block-heading lp-text-32 is-style-heading-body has-normal-font-size">Room to grow</h2><p>Scale as you go with unmetered bandwidth on all plans. Zero downtime. No limits.</p></div>
+      </section>
+      <section class="lp-block lp-card lp-pb-18">
+        <div class="lp-block lp-content lp-content--text-alignment lp-content--text-alignment-left"><h2 class="wp-block-heading is-style-heading-body has-normal-font-size">Seriously secure</h2><p>Firewalls, encryption, brute force, and DDoS protection. Security’s all taken care of.</p></div>
+      </section>
   </div>
 </template>
