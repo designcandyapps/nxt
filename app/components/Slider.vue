d9dbd663ced69d38019e5456fee2b2c16a23b3b8
Erin Dunn Halligan <erin@designcandy.com>
2025-12-31 06:34:21 -0600

    Update Slider.vue

--- app/components/Slider.vue
+++ app/components/Slider.vue
@@ -1,33 +1,8 @@
 <template>
-  <div class="news-slider slick-initialized slick-slider slick-dotted">
-
-<div>
-<p>
-<section>
-<div>
-<div>
-<div>.</div></div></div></div>
-<div>
-<h2>Room to grow</h2>
-<p>Scale as you go with unmetered bandwidth on all plans. Zero downtime. No limits.</p>
-</div>
-</section>
-</p>
-</div>
-
-<div>
-<p>
-<section>
-<div>
-<div>
-<div>.</div></div></div></div>
-<div>
-<h2>Room to grow</h2>
-<p>Scale as you go with unmetered bandwidth on all plans. Zero downtime. No limits.</p>
-</div>
-</section>
-</p>
-</div>
-
-</div>
+    <div class="news-slider">
+    <div><p></p></div>
+    <div><p></p></div>
+    <div><p></p></div>
+    <div><p></p></div>
+  </div>
 </template>
