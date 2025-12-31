5980d266b45da95954f88e9435189d65ab82f1be
Erin Dunn Halligan <erin@designcandy.com>
2025-12-31 12:06:20 -0600

    Update Slider.vue

--- app/components/Slider.vue
+++ app/components/Slider.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="news-slider">
     <div><p>Ee</p></div>
-    <div><p></p></div>
-    <div><p></p></div>
-    <div><p></p></div>
+    <div><p>Ff</p></div>
+    <div><p>Gg</p></div>
+    <div><p>Hh</p></div>
   </div>
 </template>
