a234faffd4ea15c94b20a4be301b9ec0efdbed58
Erin Dunn Halligan <erin@designcandy.com>
2025-12-31 06:32:08 -0600

    Update Slider.vue

--- app/components/Slider.vue
+++ app/components/Slider.vue
@@ -6,7 +6,7 @@
 <section>
 <div>
 <div>
-<div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3.445 16.505a.75.75 0 001.06.05l5.005-4.55 4.024 3.521 4.716-4.715V14h1.5V8.25H14v1.5h3.19l-3.724 3.723L9.49 9.995l-5.995 5.45a.75.75 0 00-.05 1.06z"></path></svg></div></div></div></div>
+<div>.</div></div></div></div>
 <div>
 <h2>Room to grow</h2>
 <p>Scale as you go with unmetered bandwidth on all plans. Zero downtime. No limits.</p>
@@ -20,7 +20,7 @@
 <section>
 <div>
 <div>
-<div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3.445 16.505a.75.75 0 001.06.05l5.005-4.55 4.024 3.521 4.716-4.715V14h1.5V8.25H14v1.5h3.19l-3.724 3.723L9.49 9.995l-5.995 5.45a.75.75 0 00-.05 1.06z"></path></svg></div></div></div></div>
+<div>.</div></div></div></div>
 <div>
 <h2>Room to grow</h2>
 <p>Scale as you go with unmetered bandwidth on all plans. Zero downtime. No limits.</p>
