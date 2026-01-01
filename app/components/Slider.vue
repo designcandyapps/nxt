7ef1b5d1960ae1824df42634086b60eda868137c
Erin Dunn Halligan <erin@designcandy.com>
2026-01-01 07:41:54 -0600

    Shorten headings in Slider component

--- app/components/Slider.vue
+++ app/components/Slider.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="news-slider">
       <section class="lp-block lp-card lp-pb-18">
-        <div class="lp-block lp-content lp-content--text-alignment lp-content--text-alignment-left"><h2 class="wp-block-heading lp-text-32 is-style-heading-body has-normal-font-size">Room to grow</h2><p>Scale as you go with unmetered bandwidth on all plans. Zero downtime. No limits.</p></div>
+        <div class="lp-block lp-content lp-content--text-alignment lp-content--text-alignment-left"><h2 class="wp-block-heading lp-text-32 is-style-heading-body has-normal-font-size">Room</h2><p>Scale as you go with unmetered bandwidth on all plans.</p></div>
       </section>
       <section class="lp-block lp-card lp-pb-18">
-        <div class="lp-block lp-content lp-content--text-alignment lp-content--text-alignment-left"><h2 class="wp-block-heading is-style-heading-body has-normal-font-size">Seriously secure</h2><p>Firewalls, encryption, brute force, and DDoS protection. Security’s all taken care of.</p></div>
+        <div class="lp-block lp-content lp-content--text-alignment lp-content--text-alignment-left"><h2 class="wp-block-heading is-style-heading-body has-normal-font-size">Seriously</h2><p>Firewalls, encryption, brute force, and DDoS protection.</p></div>
       </section>
   </div>
 </template>
