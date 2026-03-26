bc0efa0a9f62ae6fb95eec3fea1b53b7664e6f71
Erin Dunn Halligan <erin@designcandy.com>
2026-03-26 09:55:54 -0500

    Update Slider2.vue

--- app/components/Slider2.vue
+++ app/components/Slider2.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="news-slider">
       <section>
-        <div><h2>Room</h2><p>Scale as you go with unmetered bandwidth on all plans.</p></div>
+        <div class="lp-block lp-content lp-content--text-alignment lp-content--text-alignment-left"><h2>Room</h2><p>Scale as you go with unmetered bandwidth on all plans.</p></div>
       </section>
       <section>
         <div><h2>Seriously</h2><p>Firewalls, encryption, brute force, and DDoS protection.</p></div>
