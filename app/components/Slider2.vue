29d56a74aaf509e6c84777a739b12a695d686029
Erin Dunn Halligan <erin@designcandy.com>
2026-03-26 09:54:39 -0500

    Update Slider2.vue with headings and paragraphs

--- app/components/Slider2.vue
+++ app/components/Slider2.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="news-slider">
       <section>
-        <div>Scale as you go with unmetered bandwidth on all plans.</div>
+        <div><h2>Room</h2><p>Scale as you go with unmetered bandwidth on all plans.</p></div>
       </section>
       <section>
-        <div>Firewalls, encryption, brute force, and DDoS protection.</div>
+        <div><h2>Seriously</h2><p>Firewalls, encryption, brute force, and DDoS protection.</p></div>
       </section>
   </div>
 </template>
