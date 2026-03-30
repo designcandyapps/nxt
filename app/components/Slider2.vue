f1b25bbde2c1ead016773a84c26f79d036421f25
Erin Dunn Halligan <erin@designcandy.com>
2026-03-30 08:01:18 -0500

    Change class name from 'lp-content' to 'eer'

--- app/components/Slider2.vue
+++ app/components/Slider2.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="news-slider">
       <section class="lp-block lp-card lp-pb-18">
-        <div class="lp-block lp-content lp-content--text-alignment lp-content--text-alignment-left"><h2 class="wp-block-heading lp-text-32 is-style-heading-body has-normal-font-size">Room</h2><p>Scale as you go with unmetered bandwidth on all plans.</p></div>
+        <div class="eer lp-block lp-content lp-content--text-alignment lp-content--text-alignment-left"><h2 class="wp-block-heading lp-text-32 is-style-heading-body has-normal-font-size">Room</h2><p>Scale as you go with unmetered bandwidth on all plans.</p></div>
       </section>
       <section class="lp-block lp-card lp-pb-18">
         <div class="lp-block lp-content lp-content--text-alignment lp-content--text-alignment-left"><h2 class="wp-block-heading lp-text-32 is-style-heading-body has-normal-font-size">Seriously</h2><p>Firewalls, encryption, brute force, and DDoS protection.</p></div>
