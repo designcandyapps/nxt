ebdfb8f224e39521398a11b6074fe7105f5fe909
Erin Dunn Halligan <erin@designcandy.com>
2026-03-30 13:12:36 -0500

    Update Slider2.vue

--- app/components/Slider2.vue
+++ app/components/Slider2.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="news-slider">
+    <div class="znews-slider">
       <section class="e1 lp-block lp-card lp-pb-18">
         <div class="eer lp-block lp-content lp-content--text-alignment lp-content--text-alignment-left"><h2 class="wp-block-heading lp-text-32 is-style-heading-body has-normal-font-size">Room</h2><p>Scale as you go with unmetered bandwidth on all plans.</p></div>
       </section>
