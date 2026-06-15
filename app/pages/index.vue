7a170ce0956e0726fa49389f6058f8c6bff3ca8e
Erin Dunn Halligan <erin@designcandy.com>
2026-06-14 21:17:18 -0500

    Add alert for element selection in send method

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -60,6 +60,7 @@ export default{
     async send(){
       const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector("#prompt").value})});
       const data=await response.json(); this.response=data.reply; //alert("RES00: "+JSON.stringify(data)); alert("RES01: "+this.response); //console.log(data.message.content);
+      alert("T: "+document.querySelector("#t"));
       document.querySelector("#t").innerText=this.response;
     },
     async send2(){
