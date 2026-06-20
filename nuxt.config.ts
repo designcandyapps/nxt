f2e8f25c312bd06b117c03d2f75cd3a2ef0b9465
Erin Dunn Halligan <erin@designcandy.com>
2026-06-20 15:54:28 -0500

    Update script source URL in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
       {src:'https://pinfluents.com/_BCK/4/inc/zo/sw.js'},
       {src:"https://pinfluents.com/_BCK/4/inc/sl.js"},
       //{src:"https://pinfluents.com/_BCK/4/jpADGb.js"},
-      {src:'https://pinfluents.com/_BCK/4/jqAE-x.js'},
+      {src:'https://pinfluents.com/_BCK/4/jqADGb-2js'},
       //{src:'https://js.stripe.com/v3/'},
       //{src:'https://pinfluents.com/_BCK/4/inc/utils.js'},
       //{src:'https://pinfluents.com/_BCK/4/inc/index.js'}, 
