8d6bfb6d09c21b844f53a286b90d57c83b3d0fc3
Erin Dunn Halligan <erin@designcandy.com>
2026-06-21 05:25:11 -0500

    Update ColorThief.vue

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -22,22 +22,21 @@ function createScientificPalettes(baseColor){const targetHueSteps={analogous:[0,
 function discoverPalettes(colors){const palettes={}; for(const color of colors){const targetPalettes=createScientificPalettes(color); for(const paletteType of Object.keys(targetPalettes)){const palette=[]; for(const targetColor of targetPalettes[paletteType]){const availableColors=colors.filter((c)=>!palette.some((existing)=>isColorEqual(c,existing))); const match=nearest(availableColors,differenceEuclidean("lch"))(targetColor)[0]; palette.push(match)} palettes[paletteType]={colors:palette}}} return palettes}
 function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 
-/*function mobileOnlySlider($slidername,$dots,$arrows,$breakpoint){
-  var slider=$($slidername); //alert("Tzy: "+slider.html());
+function mobileOnlySlider($slidername,$dots,$arrows,$breakpoint){
+  var slider=$($slidername); alert("Tzy: "+slider.html());
   var settings={mobileFirst:true,dots:$dots,arrows:$arrows,responsive:[{breakpoint:$breakpoint,settings:"unslick"}]};slider.slick(settings);
   $(window).on("resize",function(){
     if($(window).width()>$breakpoint)return;
     if(!slider.hasClass("slick-initialized"))return slider.slick(settings);
   });
-}*/
+}
 onMounted(()=>{
   window.onload=function(){
     setTimeout(function(){
-      //alert(document.getElementById("ee").src);
-      //alert(imageurl);
+      alert(document.getElementById("ee").src); //alert(imageurl);
       imageUrl.value=document.getElementById("ee").src;
       generatePalette();
-      //mobileOnlySlider(".ey",true,false,767);
+      mobileOnlySlider(".ey",true,false,767);
     },1800);
   }
 });
