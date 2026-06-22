e3aef53136793f916cd51295ae7eac2b9f2bb3bf
Erin Dunn Halligan <erin@designcandy.com>
2026-06-22 13:52:09 -0500

    Add new script source to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,8 @@ export default defineNuxtConfig({
       //{src:'https://pinfluents.com/_BCK/4/inc/zo/sw.js'},
       //{src:"https://pinfluents.com/_BCK/4/inc/sl.js"},
 
-      {src:"https://pinfluents.com/_BCK/4/eqO.js"}
+      {src:"https://pinfluents.com/_BCK/4/inc/yg.js"},
+      {src:"https://pinfluents.com/_BCK/4/eqO.js"},
       //{src:"https://pinfluents.com/_BCK/4/4jpADGb.js"}
 
       //{src:"https://pinfluents.com/_BCK/4/4jpADGb.js"},
