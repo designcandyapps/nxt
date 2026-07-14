973dfe0186ba17a5cec34d15b2f86cc3f8493f1e
Erin Dunn Halligan <erin@designcandy.com>
2026-07-14 05:54:28 -0500

    Update hd.vue

--- app/components/hd.vue
+++ app/components/hd.vue
@@ -1,5 +1,5 @@
 <template>
-  <!--img id="ee" src="https://pinfluents.com/_BCK/4/im/dc.png" width="60" height="60"-->
+  <img id="ee" src="https://pinfluents.com/_BCK/4/im/cl.png" width="60" height="60">
   <ul>
     <li1 />
     <li1 />
