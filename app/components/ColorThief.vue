caa5603402a073ab261286ef8b48679eba91ae69
Erin Dunn Halligan <erin@designcandy.com>
2026-07-14 18:36:47 -0500

    Update ColorThief.vue

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -3,7 +3,7 @@ import {ref,onMounted} from "vue"; import ColorThief from "colorthief"; import {
 const imageUrl=ref(""); const proxyUrl=ref(""); const palette=ref([]); const backgroundImage=ref(""); const toLCH=converter("lch"); const isLoading=ref(false);
 
 const generatePalette=async()=>{
-  $('<img id="ee" class="zz" src="https://pinfluents.com/_BCK/4/im/dc.png" width="60" height="60">').insertBefore("ul:eq(0)");
+  $('<img id="ee" class="zz" src="https://pinfluents.com/_BCK/4/im/bp.png" width="60" height="60">').insertBefore("ul:eq(0)");
   imageUrl.value=document.getElementById("ee").src;
   //alert("IU1: "+imageUrl.value);
 
