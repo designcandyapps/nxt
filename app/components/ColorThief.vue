541ab2691216610f4f861fa177493862ff2b4fc8
Erin Dunn Halligan <erin@designcandy.com>
2026-07-14 23:29:45 -0500

    Update ColorThief.vue

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -3,7 +3,7 @@ import {ref,onMounted} from "vue"; import ColorThief from "colorthief"; import {
 const imageUrl=ref(""); const proxyUrl=ref(""); const palette=ref([]); const backgroundImage=ref(""); const toLCH=converter("lch"); const isLoading=ref(false);
 
 const generatePalette=async()=>{
-  $("ul:eq(0)").prepend('<img id="ee" src="https://pinfluents.com/_BCK/4/im/kw.png" width="60" height="60">');
+  //$("ul:eq(0)").prepend('<img id="ee" src="https://pinfluents.com/_BCK/4/im/kw.png" width="60" height="60">');
   imageUrl.value=document.getElementById("ee").src;
   //alert("IU1: "+imageUrl.value);
 
@@ -32,19 +32,10 @@ function createScientificPalettes(baseColor){const targetHueSteps={analogous:[0,
 function discoverPalettes(colors){const palettes={}; for(const color of colors){const targetPalettes=createScientificPalettes(color); for(const paletteType of Object.keys(targetPalettes)){const palette=[]; for(const targetColor of targetPalettes[paletteType]){const availableColors=colors.filter((c)=>!palette.some((existing)=>isColorEqual(c,existing))); const match=nearest(availableColors,differenceEuclidean("lch"))(targetColor)[0]; palette.push(match)} palettes[paletteType]={colors:palette}}} return palettes}
 function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 
-function mobileOnlySlider($slidername,$dots,$arrows,$breakpoint){
-  var slider=$($slidername); //alert("Tzy: "+slider.html());
-  var settings={mobileFirst:true,dots:$dots,arrows:$arrows,responsive:[{breakpoint:$breakpoint,settings:"unslick"}]};slider.slick(settings);
-  $(window).on("resize",function(){
-    if($(window).width()>$breakpoint)return;
-    if(!slider.hasClass("slick-initialized"))return slider.slick(settings);
-  });
-}
 onMounted(()=>{
   window.onload=function(){
     setTimeout(function(){
       generatePalette();
-      //mobileOnlySlider(".ey",true,false,767);
     },1800);
   }
 });
