778d93ec6821dc543c3cab4372122d8fa5feddd9
Erin Dunn Halligan <erin@designcandy.com>
2026-07-16 07:40:09 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -16,10 +16,6 @@ async function fetchGetty(query){
 }
 onMounted(()=>{
   //setTimeout(function(){
-    //const sc1a=document.createElement('script'); sc1a.src='https://pinfluents.com/_BCK/4/inc/zo/sw.js';
-    const sc1=document.createElement('script'); sc1.src='https://pinfluents.com/_BCK/4/inc/zo/gl.js';
-    const sc2=document.createElement('script'); sc2.src='https://pinfluents.com/_BCK/4/inc/zo/sl2.js';
-    document.head.appendChild(sc1); document.head.appendChild(sc2); //document.head.appendChild(sc1a);
     const prompt=document.querySelector("#prompt").value; //alert("PR1: "+prompt);
     const pr2=document.querySelector("#pr2").value; //alert("PR2: "+pr2);
     const pr3=document.querySelector("#pr3").value; //alert("PR3: "+pr3);
@@ -55,6 +51,10 @@ onMounted(()=>{
 export default{
   data(){return{prompt:"",response:null}},
   mounted(){
+    //const sc1a=document.createElement('script'); sc1a.src='https://pinfluents.com/_BCK/4/inc/zo/sw.js';
+    const sc1=document.createElement('script'); sc1.src='https://pinfluents.com/_BCK/4/inc/zo/gl.js';
+    const sc2=document.createElement('script'); sc2.src='https://pinfluents.com/_BCK/4/inc/zo/sl2.js';
+    document.head.appendChild(sc1); document.head.appendChild(sc2); //document.head.appendChild(sc1a);
     //setTimeout(()=>{this.send()},7800);
     //setTimeout(()=>{this.send2()},8800);
   },
