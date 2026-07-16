31220863b580249604cdc8d391bf67b33f4f1d8f
Erin Dunn Halligan <erin@designcandy.com>
2026-07-16 07:51:02 -0500

    Remove external script loading from index.vue
    Removed script loading for external resources in mounted lifecycle hook.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -51,10 +51,6 @@ onMounted(()=>{
 export default{
   data(){return{prompt:"",response:null}},
   mounted(){
-    //const sc1a=document.createElement('script'); sc1a.src='https://pinfluents.com/_BCK/4/inc/zo/sw.js';
-    const sc1=document.createElement('script'); sc1.src='https://pinfluents.com/_BCK/4/inc/zo/gl.js';
-    const sc2=document.createElement('script'); sc2.src='https://pinfluents.com/_BCK/4/inc/zo/sl2.js';
-    document.head.appendChild(sc1); document.head.appendChild(sc2); //document.head.appendChild(sc1a);
     //setTimeout(()=>{this.send()},7800);
     //setTimeout(()=>{this.send2()},8800);
   },
