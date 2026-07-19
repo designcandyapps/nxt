8a33b6118b7ce3200d53d0eedc2d824e2458031f
Erin Dunn Halligan <erin@designcandy.com>
2026-07-19 12:43:58 -0500

    Comment out unused script imports in nuxt.config.ts
    Commented out several script imports for better clarity.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,14 +17,14 @@ export default defineNuxtConfig({
   app: {head:{
     //link:[{rel:"stylesheet",href:"https://pinfluents.com/_BCK/4/inc/ct.css"}],
     script:[
-      //{src:"https://pinfluents.com/inc/jq.js"},
+      ////{src:"https://pinfluents.com/inc/jq.js"},
       {src:"https://pinfluents.com/_BCK/4/jq.js"},
-      //{src:"https://pinfluents.com/inc/colorthief.js"},
-      {src:'https://pinfluents.com/_BCK/4/inc/zo/ap.js'},
-      //{src:"https://pinfluents.com/_BCK/4/inc/zo/sw.js"},
-      {src:"https://pinfluents.com/_BCK/4/inc/sl.js"},
-      //{src:"https://pinfluents.com/_BCK/4/jq0.js"},
-      {src:"https://pinfluents.com/_BCK/4/jqP8-e.js"},
+      ////{src:"https://pinfluents.com/inc/colorthief.js"},
+      //{src:'https://pinfluents.com/_BCK/4/inc/zo/ap.js'},
+      ////{src:"https://pinfluents.com/_BCK/4/inc/zo/sw.js"},
+      //{src:"https://pinfluents.com/_BCK/4/inc/sl.js"},
+      ////{src:"https://pinfluents.com/_BCK/4/jq0.js"},
+      //{src:"https://pinfluents.com/_BCK/4/jqP8-e.js"},
 
       //{src:"https://pinfluents.com/_BCK/4/inc/zo/gl.js",body:true},
       //{src:"https://pinfluents.com/_BCK/4/inc/zo/sl2.js",body:true},
