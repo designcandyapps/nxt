c6bbaddbbf78a2f4a632e44f67c3c36e3f04b3e0
Erin Dunn Halligan <erin@designcandy.com>
2026-07-19 17:46:34 -0500

    Update ColorThief.vue

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -4,8 +4,8 @@ const imageUrl=ref(""); const proxyUrl=ref(""); const palette=ref([]); const bac
 
 const generatePalette=async()=>{
   alert(1);
-  //imageUrl.value=document.getElementById("ee").src;
-  //alert("IU1: "+imageUrl.value);
+  imageUrl.value=document.getElementById("ee").src;
+  alert("IU1: "+imageUrl.value);
 
   isLoading.value=true; proxyUrl.value=`/api/proxy?url=${encodeURIComponent(imageUrl.value)}`;
   //alert("PU1: "+proxyUrl.value);
@@ -41,8 +41,6 @@ onMounted(()=>{
   window.onload=function(){
     //document.querySelector(".ul").prepend('<img id="ee" src="https://pinfluents.com/_BCK/4/im/gr.png" width="60" height="60">'); //$("ul:eq(0)")
     setTimeout(function(){
-      imageUrl.value=document.getElementById("ee").src;
-      alert("IU1: "+imageUrl.value);
       generatePalette();
     },1800);
   }
