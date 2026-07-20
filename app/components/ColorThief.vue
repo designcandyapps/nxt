1157744b2966e7f160d4e28ce53684a166442190
Erin Dunn Halligan <erin@designcandy.com>
2026-07-20 07:58:57 -0500

    Update ColorThief.vue

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -4,7 +4,7 @@ const imageUrl=ref(""); const proxyUrl=ref(""); const palette=ref([]); const bac
 
 const generatePalette=async()=>{
   imageUrl.value=document.getElementById("ee").src;
-  //alert("IU1: "+imageUrl.value);
+  alert("IU1: "+imageUrl.value);
 
   isLoading.value=true; proxyUrl.value=`/api/proxy?url=${encodeURIComponent(imageUrl.value)}`;
   //alert("PU1: "+proxyUrl.value);
