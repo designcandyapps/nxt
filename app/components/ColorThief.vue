4472233b79089948b0651d981c75559ee6c1456d
Erin Dunn Halligan <erin@designcandy.com>
2026-07-20 06:53:55 -0500

    Update ColorThief.vue

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -3,14 +3,14 @@ import {ref,onMounted} from "vue"; import ColorThief from "colorthief"; import {
 const imageUrl=ref(""); const proxyUrl=ref(""); const palette=ref([]); const backgroundImage=ref(""); const toLCH=converter("lch"); const isLoading=ref(false);
 
 const generatePalette=async()=>{
-  //alert("YY: "+document.querySelector(".yy").src);
-  imageUrl.value=document.querySelector("#ee").src;
+  //document.querySelector(".provider-header").prepend('<img id="ee" src="https://pinfluents.com/_BCK/4/im/gr.png" width="60" height="60">'); //$("ul:eq(0)")
+  imageUrl.value=document.getElementById("ee").src;
   //alert("IU1: "+imageUrl.value);
 
   isLoading.value=true; proxyUrl.value=`/api/proxy?url=${encodeURIComponent(imageUrl.value)}`;
   //alert("PU1: "+proxyUrl.value);
   const img=new Image(); img.crossOrigin="Anonymous"; img.src=proxyUrl.value;
-  //alert("IS: "+img.src);
+  alert("PU2: "+proxyUrl.value);
 
   img.onload=()=>{const colorThief=new ColorThief(); let colors=colorThief.getPalette(img).map((c)=>toLCH({r:c[0]/255,g:c[1]/255,b:c[2]/255,mode:"rgb"}));
     const palettesz=discoverPalettes(colors); document.getElementById("z").innerHTML=`<span class="content"></span>`;
@@ -21,7 +21,15 @@ const generatePalette=async()=>{
     const scientificColors=discoverPalettes(colors); palette.value=Object.keys(scientificColors).map((type)=>({type,colors:scientificColors[type].colors.map((color)=>({hex:formatHex(color)}))}));
     backgroundImage.value=`url('${imageUrl.value}')`; isLoading.value=false
   };
-  alert("DV7: "+document.querySelector("#dv7"));
+  var r0=document.querySelector("#dv7").style.backgroundColor;
+  alert("R02: "+r0);
+
+  r0=r0&&r0=="rgb(0, 0, 0)"?"#000011":r0;
+  var r1="rgba("+r0.replace("rgb(","").replace(")","")+",.1)"; var r1a="rgba(255,255,255,1)";
+  var r2=document.querySelector("#dv8").style.backgroundColor; r2=r2&&r2=="rgb(0, 0, 0)"?"#001100":"#e7e7e7"; //0c152d
+  var r3=document.querySelector("#dv10").style.backgroundColor; r3="rgba("+r2.replace("rgb(","").replace(")","")+",.75)";
+  var r4="rgba("+r2.replace("rgb(","").replace(")","")+",.85)";
+  var r5="rgba("+r3.replace("rgb(","").replace(")","")+",.9)";
   //document.body.style.backgroundColor=document.querySelector("#dv7").style.backgroundColor;
   img.onerror=()=>{console.error("Failed to Load"); isLoading.value=false}
 };
@@ -32,11 +40,8 @@ function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 onMounted(()=>{
   window.onload=function(){
     setTimeout(function(){
-      ////document.querySelector(".ul").prepend('<img id="ee" src="https://pinfluents.com/_BCK/4/im/gr.png" width="60" height="60">'); //$("ul:eq(0)") //document.querySelector(".bb")
-      //document.querySelector(".bb").innerHTML='<img id="ee" class="yy" src="https://pinfluents.com/_BCK/4/im/hn.png" width="60" height="60">';
-      //document.querySelector(".yy").src="https://pinfluents.com/_BCK/4/im/bp.png";
       generatePalette();
-    },8800);
+    },1800);
   }
 });
 </script>
