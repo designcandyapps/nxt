d9f1bad78cc03a34d87f41a024960c9b431319ca
Erin Dunn Halligan <erin@designcandy.com>
2026-07-21 07:34:22 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,19 +15,16 @@ export default defineNuxtConfig({
     }
   },
   app: {head:{
-    //link:[{rel:"stylesheet",href:"https://pinfluents.com/_BCK/4/inc/ct.css"}],
+    link:[{rel:"stylesheet",href:"https://pinfluents.com/_BCK/4/inc/ct.css"}],
     script:[
       //{src:"https://pinfluents.com/inc/jq.js"},
       {src:"https://pinfluents.com/_BCK/4/jq.js"},
       {src:"https://pinfluents.com/inc/colorthief.js"},
       {src:'https://pinfluents.com/_BCK/4/inc/zo/ap.js'},
-      ////{src:"https://pinfluents.com/_BCK/4/inc/zo/sw.js"},
+      {src:"https://pinfluents.com/_BCK/4/inc/zo/sw.js"},
       {src:"https://pinfluents.com/_BCK/4/inc/sl.js"},
-      ////{src:"https://pinfluents.com/_BCK/4/jq0.js"},
-      {src:"https://pinfluents.com/_BCK/4/jqP12-e.js"},
-
-      //{src:"https://pinfluents.com/_BCK/4/inc/zo/gl.js",body:true,defer:true},
       {type:'text/javascript',src:'https://pinfluents.com/_BCK/4/inc/zo/gl.js',body:true,defer:true,tagPosition:'bodyClose'}
+      {src:"https://pinfluents.com/_BCK/4/jqP12-e.js"},
       
       //{src:'https://js.stripe.com/v3/'},
       //{src:'https://pinfluents.com/_BCK/4/inc/utils.js'},
