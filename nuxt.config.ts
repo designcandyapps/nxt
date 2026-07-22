3e6594f4e515f11f58f4932849dc756c51b6f3b0
Erin Dunn Halligan <erin@designcandy.com>
2026-07-22 09:36:34 -0500

    Uncomment jQuery script inclusion in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
   app: {head:{
     //link:[{rel:"stylesheet",href:"https://pinfluents.com/_BCK/4/inc/ct.css"}],
     script:[
-      //{src:"https://pinfluents.com/_BCK/4/jq.js"},
+      {src:"https://pinfluents.com/_BCK/4/jq.js"},
       //{src:"https://pinfluents.com/inc/colorthief.js"},
       //{src:"https://pinfluents.com/_BCK/4/inc/sl.js"},
       //{src:'https://pinfluents.com/_BCK/4/gl.js',class:'st'},
