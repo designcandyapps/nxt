496af04536e6003e37a3b81e87be4453a6187849
Erin Dunn Halligan <erin@designcandy.com>
2026-07-22 16:27:09 -0500

    Update ColorThief.vue

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -32,8 +32,8 @@ function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 
 onMounted(()=>{
   alert(0);
-  //setTimeout(function(){
+  setTimeout(function(){
     generatePalette();
-  //},800);
+  },1200);
 });
 </script>
