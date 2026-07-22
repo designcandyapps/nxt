1908d1e896ff8a7714a87f768d19698a4212862f
Erin Dunn Halligan <erin@designcandy.com>
2026-07-22 16:04:05 -0500

    Update hd.vue

--- app/components/hd.vue
+++ app/components/hd.vue
@@ -1,13 +1,5 @@
 <template>
-  <input id="t" v-model="t">
-  <span class="g">
-    <span id="z"><ColorThief /></span><input id="prompt" v-model="prompt"><input id="pho" v-model="pho"><input id="pho2" v-model="pho2">
-    <input id="pr2" v-model="pr2"><br><input id="pr3" v-model="pr3"><br><input id="pr4" v-model="pr4"><br><input id="pr5" v-model="pr5">
-    <span id="response" v-if="response">{{response}}</span>
-  </span>
-  <header>
   <ul>
     <li1 />
   </ul>
-  </header>
 </template>
