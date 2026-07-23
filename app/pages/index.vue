fdebdc12fea8e9c8c82519b9d63199cc8335a539
Erin Dunn Halligan <erin@designcandy.com>
2026-07-23 18:52:39 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -17,13 +17,12 @@ async function fetchGetty(query){
 onMounted(()=>{
   //setTimeout(function(){
     //const pr="cars";
-    const pr:string=new URLSearchParams(location.search).get("pr")??document.getElementById("prompt").value;
-    alert("P111: "+pr);
+    const pr:string=new URLSearchParams(location.search).get("pr")??"trees";
     //pr=pr&&p!=""?pr:document.getElementById("prompt").value;
     document.getElementById("tr").innerText=pr; document.getElementById("prompt").value=document.getElementById("tr").innerText;
 
     //alert("PROMPT: "+document.querySelector("#prompt").value);
-    alert("PRP: "+document.getElementById("tr").innerText);
+    //alert("PRP: "+document.getElementById("tr").innerText);
 
     const prompt=document.querySelector("#prompt").value; alert("PROMPT2: "+prompt);
     const prp=document.getElementById("tr").innerText; alert("PRP2: "+prp);
