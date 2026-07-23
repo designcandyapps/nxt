79da7d42edcb43681097bb20d8fabaa36763c2c2
Erin Dunn Halligan <erin@designcandy.com>
2026-07-23 16:13:11 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,7 +10,7 @@ async function fetchGetty(query){
   try{
     const response=await fetch(`https://api.gettyimages.com/v3/search/images?phrase=${encodeURIComponent(query)}&page_size=1`,{method:"GET",headers:{"Api-Key":"ep3mq3jxr4u99m7hy3gzzp3g"}});
     if(!response.ok){throw new Error(`Error1:${response.statusText}`)}
-    const data=await response.json(); alert("RES2P: "+JSON.stringify(data));
+    const data=await response.json(); //alert("RES2P: "+JSON.stringify(data));
     if(data.images&&data.images.length>0){const image=data.images[0];console.log("Im:",image);return image}else{console.log("No ims");return null}
   }catch(error){console.error("Error2:",error)}
 }
@@ -26,8 +26,8 @@ onMounted(()=>{
     const pr3=document.querySelector("#pr3").value; //alert("PR3: "+pr3);
     const pr4=document.querySelector("#pr4").value; //alert("PR4: "+pr4);
     const pr5=document.querySelector("#pr5").value; //alert("PR5: "+pr5);*/
-    //fetchPh(prp).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small})});
-    fetchGetty(prp).then(image=>{pho2.value=image.display_sizes[0].uri});
+    fetchPh(prp).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small})});
+    //fetchGetty(prp).then(image=>{pho2.value=image.display_sizes[0].uri});
   },3800);
 });
 </script>
