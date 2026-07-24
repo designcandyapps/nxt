d229a314e9201dac7565cb124e0cd539e4509b5c
Erin Dunn Halligan <erin@designcandy.com>
2026-07-23 19:20:05 -0500

    Comment out prompt value logic in index.vue
    Commented out code related to setting prompt value and alerting PR.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -17,9 +17,9 @@ async function fetchGetty(query){
 onMounted(()=>{
   //setTimeout(function(){
     //const pr="cars";
-    document.getElementById("prompt").value=document.getElementById("tr").innerText;
-    const pr:string=new URLSearchParams(location.search).get("pr")??document.getElementById("prompt").value;
-    pr=pr&&pr!="Innovation for the energy of today and tomorrow"?pr:document.getElementById("prompt").value;
+    //document.getElementById("prompt").value=document.getElementById("tr").innerText;
+    //const pr:string=new URLSearchParams(location.search).get("pr")??document.getElementById("prompt").value;
+    //pr=pr&&pr!="Innovation for the energy of today and tomorrow"?pr:document.getElementById("prompt").value;
     alert("PR: "+pr);
   
     //const pr:string=new URLSearchParams(location.search).get("pr")??"trees"; //pr=pr&&pr!=""?pr:document.getElementById("prompt").value;
