ce52a9d8dfe72658e9b11997c8c4b76116526b4b
Erin Dunn Halligan <erin@designcandy.com>
2026-07-23 19:17:34 -0500

    Fix selector for updating text in index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -71,7 +71,7 @@ export default{
       const data=await response.json(); this.response=data.reply; alert("RES00: "+JSON.stringify(data)); alert("RES01: "+this.response); //console.log(data.message.content);
       //alert("T1: "+document.querySelector("#tr").innerText);
       document.querySelector("#h1n").innerText=this.response;
-      document.getElementById("tr").innerText=prompt;
+      document.querySelector("#tr").innerText=prompt;
       //->document.querySelector("#tr").innerText=pr;
     },
     async send2(){
