f5ec2bb9d34c4e3beb61fa56fcf6d32c28315a98
Erin Dunn Halligan <erin@designcandy.com>
2026-08-13 23:20:25 -0500

    Update ColorThief.vue

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -24,6 +24,7 @@ const generatePalette=async()=>{
 
     const scientificColors=discoverPalettes(colors); palette.value=Object.keys(scientificColors).map((type)=>({type,colors:scientificColors[type].colors.map((color)=>({hex:formatHex(color)}))}));
     backgroundImage.value=`url('${imageUrl.value}')`; isLoading.value=false;
+
     const r0=document.querySelector("#dv7").style.backgroundColor; alert("G: "+r0);
     const r2=document.querySelector("#dv8").style.backgroundColor; //r2=r2!=""?r2.replace("rgb(","rgba(").replace(")",",.1)"):"#001100"; //a9a9a9
     const r3=document.querySelector("#dv10").style.backgroundColor; //r3=r3!=""?r3.replace("rgb(","rgba(").replace(")",",.75)"):"red";
