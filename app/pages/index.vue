ce2eccd3b9422ae6aa776a0c5e6b588251e51dcf
Erin Dunn Halligan <erin@designcandy.com>
2026-08-14 01:34:23 -0500

    Modify photo fetch alert in index.vue
    Updated alert to show the last photo URL after fetching.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -34,10 +34,12 @@ onMounted(()=>{
     const pr3=document.querySelector("#pr3").value; //alert("PR3: "+pr3);
     const pr4=document.querySelector("#pr4").value; //alert("PR4: "+pr4);
     const pr5=document.querySelector("#pr5").value; //alert("PR5: "+pr5);*/
-    fetchPh(prompt).then(photos=>{photos.forEach(photo=>{
-      pho.value=photo.urls.small;
-      alert(pho[0].value);
-    })});
+    fetchPh(prompt).then(photos=>{
+      photos.forEach(photo=>{
+        pho.value=photo.urls.small;
+      });
+      alert("PH: "+pho.value);
+    });
     //fetchGetty(prp).then(image=>{pho2.value=image.display_sizes[0].uri});
   //},7800);
 });
