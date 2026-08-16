d740537cb1cd86f1c44724ce8dab030d0d9f2554
Erin Dunn Halligan <erin@designcandy.com>
2026-08-16 12:44:53 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 const {data:page}=await useAsyncData('index',()=>queryContent('/').findOne());
 useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,description:page.value.description,ogDescription:page.value.description});
-import {ref,onMounted} from "vue"; const proxyUrl=ref(""); //const pr2=document.querySelector("#pr2");
+import {ref,onMounted} from "vue"; const proxyUrl=ref(""); const pr2=document.querySelector("#pr2");
 const genTktlr=async()=>{
   alert(2);
-  proxyUrl.value=`/api/proxy?url=${encodeURIComponent(document.querySelector("#pr2").value)}`; //pr2.value
+  proxyUrl.value=`/api/proxy?url=${encodeURIComponent(pr2.value)}`; //document.querySelector("#pr2").value
   alert("PRX: "+proxyUrl.value);
 };
 const fetchPh=async(query)=>{
