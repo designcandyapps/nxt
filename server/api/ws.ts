52d6dda482d906819be375140db83eae2ea20bae
Erin Dunn Halligan <erin@designcandy.com>
2026-09-03 01:48:20 -0500

    Uncomment URL validation in WebSocket handler

--- server/api/ws.ts
+++ server/api/ws.ts
@@ -1,6 +1,8 @@
 export default defineEventHandler(async(event)=>{
   const query=getQuery(event); const uUrl=query.url as string;
-  //if(!uUrl){return new Response("Inval",{status:400})}
+  if(!uUrl){
+    return new Response("Inval",{status:400})
+  }
   try{const response=await fetch(uUrl,{headers:{"User-Agent":"Mozilla/5.0"}});
   //if(!response.ok){return new Response("Error",{status:500})}
   const contentType=response.headers.get("Content-Type");
