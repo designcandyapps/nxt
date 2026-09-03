52b4041c452b0c3ef46a305de242355e7f6da95a
Erin Dunn Halligan <erin@designcandy.com>
2026-09-03 05:13:32 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -61,6 +61,7 @@ onMounted(()=>{
     //uUrl.value="pinfluents.com"; pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
     /////pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
     //fetchU(pUrl.value); //.then();
+    alert("UU: "+uu);
     fetchU(uu); //"wordpess.com"
   },5800);
 });
