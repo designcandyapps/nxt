5ec0275a2eb2dc2591a3de9c6601644abba2d43c
Erin Dunn Halligan <erin@designcandy.com>
2026-09-03 05:42:39 -0500

    Update pr retrieval method in index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -40,7 +40,7 @@ onMounted(()=>{
 
     //const uu:string=new URLSearchParams(location.search).get("uu")??"ign";
     //const uu=new URLSearchParams(location.search).get("uu"); uu=!uu||uu=="null"?$("#ui").val():"https://www."+uu; alert("UU: "+uu);
-    const pr:string=new URLSearchParams(location.search).get("pr")??h1; //pr=pr&&pr!=""?pr:document.getElementById("prompt").value;
+    const pr:string=new URLSearchParams(location.search).get("pr")??document.querySelector("h1").innerText; //pr=pr&&pr!=""?pr:document.getElementById("prompt").value;
     //const pr2:string=new URLSearchParams(location.search).get("pr2")??"gn";
     document.getElementById("tr").innerText=pr; document.getElementById("prompt").value=document.getElementById("tr").innerText;
     //alert("PROMPT: "+document.querySelector("#prompt").value);
