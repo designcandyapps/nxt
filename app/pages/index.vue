33a82a6d59d49162cdfeef12261a80e3058ae993
Erin Dunn Halligan <erin@designcandy.com>
2026-09-03 02:56:17 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -67,8 +67,8 @@ onMounted(()=>{
 
 
     uUrl.value="pinfluents.com";
-    pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
-    //pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
+    //pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
+    pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
     fetchU(pUrl.value); //.then();
     //fetchU("wordpess.com");
 
