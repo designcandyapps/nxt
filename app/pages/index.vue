af5e6471af134e70b1fddc26b73753dfccb7b890
Erin Dunn Halligan <erin@designcandy.com>
2026-09-03 04:44:59 -0500

    Update fetch call to retrieve images instead of h1
    Changed fetch selector from h1 to img and updated image handling.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -22,10 +22,11 @@ async function fetchGetty(query){
   }catch(error){console.error("Error2:",error)}
 }
 const fetchU=async(query)=>{
-  const response=await fetch(`https://web.scraper.workers.dev?url=${encodeURIComponent(query)}&selector=h1`);
+  const response=await fetch(`https://web.scraper.workers.dev?url=${encodeURIComponent(query)}&selector=img`);
   //const response=await fetch(`/api/ws?url=${encodeURIComponent(query)}`);
-  const data=await response.json(); //alert("RESPz: "+JSON.stringify(data));
-  const h1=data.result.h1[0]; document.querySelector("h1").innerHTML=h1;
+  const data=await response.json(); alert("RESPz: "+JSON.stringify(data));
+  //const h1=data.result.h1[0]; document.querySelector("h1").innerHTML=h1;
+  const im1=data.result.img[0].src; document.querySelector("img").src=im1;
   return data.results;
 };
 onMounted(()=>{
