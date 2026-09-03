6ee70b78d69f1bcda20b22fd271dc2ad8a1ce0b7
Erin Dunn Halligan <erin@designcandy.com>
2026-09-03 04:46:48 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -37,6 +37,7 @@ onMounted(()=>{
     //pr=pr&&pr!="Innovation for the energy of today and tomorrow"?pr:document.getElementById("prompt").value;
     //alert("PR: "+pr);
 
+    const uu:string=new URLSearchParams(location.search).get("uu");
     const pr:string=new URLSearchParams(location.search).get("pr")??"design"; //pr=pr&&pr!=""?pr:document.getElementById("prompt").value;
     //const pr2:string=new URLSearchParams(location.search).get("pr2")??"gn";
     document.getElementById("tr").innerText=pr; document.getElementById("prompt").value=document.getElementById("tr").innerText;
