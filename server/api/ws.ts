7ae29594a68475ce90d07f81d0b4f97e397f84fc
Erin Dunn Halligan <erin@designcandy.com>
2026-09-02 23:07:29 -0500

    Update ws.ts

--- server/api/ws.ts
+++ server/api/ws.ts
@@ -6,7 +6,15 @@ export default defineEventHandler(async(event)=>{
   if(!response.ok){return new Response("Error",{status:500})}
   const contentType=response.headers.get("Content-Type");
   //if(!contentType||!contentType.startsWith("image/")){return new Response("Not Img",{status:400})}
-  return new Response(await response.arrayBuffer(),{headers:{"Content-Type":contentType,"Access-Control-Allow-Origin":"*","Cache-Control":"no-cache"}})}
+  return new Response(
+    await response.arrayBuffer(),{
+      headers:{
+        "Content-Type":contentType,
+        "Access-Control-Allow-Origin":"*",
+        "Cache-Control":"no-cache"
+      }
+    }
+  )}
   catch(error){
     //return new Response("Server Error",{status:500})
   }
