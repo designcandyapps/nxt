8df7c60ccf7d10c3b797764670c10693423ae477
Erin Dunn Halligan <erin@designcandy.com>
2026-09-02 20:32:49 -0500

    Add selector parameter to scraper URL

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -77,7 +77,7 @@ onMounted(()=>{
     //handleRequest("pinfluents.com");
     //uUrl.value=document.getElementById("ee").src; alert("IU1z: "+uUrl.value);
     
-    uUrl.value="pinfluents.com"; pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}`;
+    uUrl.value="pinfluents.com"; pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
     //////uUrl.value="pinfluents.com"; pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`; //img.src=pUrl.value; alert("PU2z: "+pUrl.value);
     fetchU(pUrl.value); //.then();
     //fetchU("pinfluents.com");
