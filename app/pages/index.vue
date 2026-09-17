a438327233c3f5c9a016b238a3499e3bca2b584d
Erin Dunn Halligan <erin@designcandy.com>
2026-09-17 11:06:37 -0500

    Refactor onMounted to simplify parameter retrieval

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -34,7 +34,6 @@ onMounted(()=>{
   //setTimeout(function(){
     const pr:string=new URLSearchParams(location.search).get("pr")??document.querySelector("h1").innerText;
     document.getElementById("tr").innerText=pr; document.getElementById("prompt").value=document.getElementById("tr").innerText;
-
     const prompt=document.querySelector("#prompt").value; //alert("PROMPT: "+prompt);
     const pr2=document.querySelector("#pr2").value; //alert("PR2: "+pr2);
     const pr3=document.querySelector("#pr3").value; //alert("PR3: "+pr3);
