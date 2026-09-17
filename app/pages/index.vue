c229367464db772900c351e4cb01b59d835e4884
Erin Dunn Halligan <erin@designcandy.com>
2026-09-17 09:39:00 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -64,7 +64,7 @@ onMounted(()=>{
     //uUrl.value="pinfluents.com"; pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
     /////pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
     //fetchU(pUrl.value); //.then();
-    fetchU("wordpress.com");
+    fetchU("designcandy.com");
   },5800);
 });
 </script>
