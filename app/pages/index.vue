409ba053e38ed3b932856321b356946d00401c69
Erin Dunn Halligan <erin@designcandy.com>
2026-09-17 07:50:43 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -34,7 +34,6 @@ const fetchU=async(query)=>{
 //uUrl.value="pinfluents.com"; pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
 /////pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
 //fetchU(pUrl.value); //.then();
-fetchU(u);
 
 
 onMounted(()=>{
@@ -58,12 +57,13 @@ onMounted(()=>{
     const pr3=document.querySelector("#pr3").value; //alert("PR3: "+pr3);
     const pr4=document.querySelector("#pr4").value; //alert("PR4: "+pr4);
     const pr5=document.querySelector("#pr5").value; //alert("PR5: "+pr5);
-    alert("PR1: "+pr);
+    //alert("PR1: "+pr);
 
   setTimeout(function(){
     const pho=document.querySelector("#pho"); const pho2=document.querySelector("#pho2");
     fetchPh(prompt).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small}); /*alert("PH: "+pho.value)*/});
     //fetchGetty(prp).then(image=>{pho2.value=image.display_sizes[0].uri});
+    fetchU(u);
   },5800);
 });
 </script>
