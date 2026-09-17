3c26a94783806aba8a23de8cf5e7fc2f9f7a1dca
Erin Dunn Halligan <erin@designcandy.com>
2026-09-17 15:40:13 -0500

    Update search.json.get.ts

--- server/api/search.json.get.ts
+++ server/api/search.json.get.ts
@@ -1,4 +1,4 @@
-/*import {serverQueryContent} from '#content/server'
+import {serverQueryContent} from '#content/server'
 export default eventHandler(async(event)=>{
   return serverQueryContent(event).where({_type:'markdown',navigation:{$ne:false}}).find()
-})*/
+})
