aaff0c95f49c358a87ef10d58df415481261380d
Erin Dunn Halligan <erin@designcandy.com>
2026-09-20 17:11:59 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -44,7 +44,7 @@ onMounted(()=>{
     //<<fetchU("designcandy.com");
   //},5800);
   //setTimeout(function(){
-    const pr:string=new URLSearchParams(location.search).get("u")??document.querySelector("#prompt").value; //document.querySelector("h1").innerText
+    const pr:string=new URLSearchParams(location.search).get("u")??document.querySelector("h1").innerText; //document.querySelector("#prompt").value
     //document.getElementById("tr").innerText=pr; document.getElementById("prompt").value=document.getElementById("tr").innerText;
     //const prompt=document.querySelector("#prompt").value; const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
     alert("PR2: "+pr);
