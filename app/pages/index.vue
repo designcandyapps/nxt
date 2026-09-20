d864de78d9b6be547d2e2d34a9ad22dab32f4290
Erin Dunn Halligan <erin@designcandy.com>
2026-09-20 16:32:25 -0500

    Comment out setTimeout and related code

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -39,13 +39,14 @@ onMounted(()=>{
     //document.getElementById("t").value=document.querySelector("h1").innerText;
     //<<fetchU("designcandy.com");
   //},5800);
-  setTimeout(function(){
-    const pr:string=new URLSearchParams(location.search).get("u")??document.querySelector("h1").innerText;
+  //setTimeout(function(){
+    //const pr:string=new URLSearchParams(location.search).get("u")??document.querySelector("h1").innerText;
+    const pr=new URLSearchParams(location.search).get("u");
     //document.getElementById("tr").innerText=pr; document.getElementById("prompt").value=document.getElementById("tr").innerText;
     //const prompt=document.querySelector("#prompt").value; const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
     alert("PR: "+pr);
     fetchU(pr);
-  },7800);
+  //},7800);
 });
 </script>
 
