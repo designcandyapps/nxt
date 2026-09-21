f5d663b817fd5f1fd68cd768c01443229586ce00
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 05:36:27 -0500

    Update pUrl assignment to use API endpoint

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -7,8 +7,8 @@ const generatePalette=async()=>{alert(1);
   //prompt.value=document.querySelector("#prompt").value; alert("PR99: "+prompt.value);
   //pr.value=document.querySelector("#pr").value; pr2.value=document.querySelector("#pr2").value;
   uUrl.value="wordpress.com"; //u=!u||u=="null"?$("#pr2").val():u;
-  pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
-  //pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
+  //pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
+  pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
   alert("pUrl: "+pUrl.value);
   
   
