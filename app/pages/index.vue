ac5fb1313c42803066e6fbcaeebbb71a315aada8
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 03:10:41 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -22,9 +22,9 @@ async function fetchGetty(query){
   }catch(error){console.error("Error2:",error)}
 }
 async function fetchZ(query){
-  try{
+  //try{
     const response=await fetch(`https://web.scraper.workers.dev?url=${encodeURIComponent(query)}&selector=h1`});
-    if(!response.ok){throw new Error(`Error1:${response.statusText}`)}
+    //if(!response.ok){throw new Error(`Error1:${response.statusText}`)}
     const data=await response.json(); alert("RES3P: "+JSON.stringify(data));
     const h1=data.result.h1[0];
 
@@ -35,7 +35,7 @@ async function fetchZ(query){
     alert("PRO1: "+document.querySelector("#prompt").value);
 
     return data.results;
-  }catch(error){console.error("Error3:",error)}
+  //}catch(error){console.error("Error3:",error)}
 }
 const fetchU=async(query)=>{
   const response=await fetch(`https://web.scraper.workers.dev?url=${encodeURIComponent(query)}&selector=h1`);
