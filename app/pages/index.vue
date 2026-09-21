11400375e8ea2eb15c986fb288c5597dd549ccbc
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 07:03:30 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -83,30 +83,8 @@ onMounted(()=>{alert(8);
 });
 
 
-/*
-onMounted(()=>{
-  //setTimeout(function(){
-    //document.getElementById("tr").innerText=h1;
-    document.getElementById("prompt").value=document.getElementById("tr").innerText;
-    const prompt=document.querySelector("#prompt").value; //alert("PROMPT: "+prompt);  
-    //const pr:string=new URLSearchParams(location.search).get("u")??"wordpress.com"; //document.querySelector("#tr").innerText;
-    //const pr=new URLSearchParams(location.search).get("u"); pr=pr&&pr!=""?document.querySelector("#pr").value; //u=!u||u=="null"?$("#pr2").val():u;
-    //alert("PR: "+pr);
-  
-    //const pr=document.querySelector("#pr").value; alert("PR: "+pr);
-    //const pr2=document.querySelector("#pr2"); pr2=pr2&&pr2!=""?pr2.value; alert("PR2: "+pr2); //alert(document.querySelector("#sp"));
-    //<<const pho=document.querySelector("#pho"); const pho2=document.querySelector("#pho2");
-    //fetchPh(pr).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small}); /*alert("PH: "+pho.value)*/});
-    //fetchGetty(pr).then(image=>{pho2.value=image.display_sizes[0].uri});
 
-    uUrl.value=document.getElementById("sp").innerText;
-    pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
-    //pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
-    //alert("pUrlll: "+pUrl.value);
-    fetchU(uUrl.value);
-  //},5800);
-});
-*/
+
 </script>
 
 <template>
