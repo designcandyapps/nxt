2484b967433c90c5aaafdbe8e3308dd55b802e90
Erin Dunn Halligan <erin@designcandy.com>
2026-09-20 20:49:42 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -21,15 +21,15 @@ async function fetchGetty(query){
   }catch(error){console.error("Error2:",error)}
 }
 const fetchU=async(query)=>{
-  const response=await fetch(`https://web.scraper.workers.dev?url=${encodeURIComponent(query)}&selector=svg`);
+  const response=await fetch(`https://web.scraper.workers.dev?url=${encodeURIComponent(query)}&selector=img`);
   //const response=await fetch(`/api/ws?url=${encodeURIComponent(query)}`);
   const data=await response.json(); alert("RESPz: "+JSON.stringify(data));
   const h1=data.result.h1[0];
-  const sv=data.result.svg[0];
+  const im=data.result.img[0];
   //setTimeout(function(){
     //document.getElementById("tr").innerText=h1;
 
-    alert("SV: "+sv.innerHTML);
+    alert("IM: "+img.src);
     //document.getElementById("ee").innerText=h1.src;
   
   //},2800);
