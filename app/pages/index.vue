71a2a2f5308c4e65096f66b4be166eca935814ec
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 02:22:03 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -29,6 +29,13 @@ const fetchU=async(query)=>{
   //document.querySelector("#prompt").value=h1;
   document.getElementById("tr").innerText=h1;
   document.getElementById("prompt").value="Te"; //document.getElementById("tr").innerText;
+
+//setTimeout(function(){
+  //document.querySelector("#prompt").value=document.getElementById("tr").innerText;
+  const pro=document.querySelector("#prompt").value;                   //const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
+  alert("PRO: "+pro); //document.querySelector("#prompt").value
+  //document.getElementById("h1n").innerText=document.getElementById("response").innerText;
+//},5800);
   
   return data.results;
 };
@@ -63,12 +70,7 @@ onMounted(()=>{
   //alert("PR: "+pr);
 fetchU(pr);
 
-setTimeout(function(){
-  //document.querySelector("#prompt").value=document.getElementById("tr").innerText;
-  const pro=document.querySelector("#prompt").value;                   //const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
-  alert("PRO: "+pro); //document.querySelector("#prompt").value
-  //document.getElementById("h1n").innerText=document.getElementById("response").innerText;
-  },5800);
+/**x**/
 });
 </script>
 
