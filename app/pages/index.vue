896cda2eb6a4ff73bee18e676c2496318d939378
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 02:10:03 -0500

    Change prompt element selector in index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -64,7 +64,7 @@ onMounted(()=>{
 fetchU(pr);
   
 //document.querySelector("#prompt").value=document.getElementById("tr").innerText;
-const prompt=document.querySelector("#prompt").value;                   //const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
+const prompt=document.querySelector("#t").value;                   //const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
 alert("PROMPT: "+prompt); //document.querySelector("#prompt").value
   //document.getElementById("h1n").innerText=document.getElementById("response").innerText;
   //},5800);
