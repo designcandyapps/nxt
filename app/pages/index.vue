c13eba6557562a10bb8bfa6e3a51ef55c5e0eea8
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 08:30:34 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -57,7 +57,7 @@ const generatePalette=async()=>{alert(9);
   //fetchU(uUrl.value);
 
   const prompt=document.querySelector("#prompt").value;
-  fetchU(prompt);
+  fetchU(uUrl.value);
 
   imageUrl.value=document.getElementById("ee").src; alert("IU1: "+imageUrl.value);
   isLoading.value=true; proxyUrl.value=`/api/proxy?url=${encodeURIComponent(imageUrl.value)}`;
