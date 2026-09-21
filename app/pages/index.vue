a17d42f41fbcb1f709a09221775458a536e1364f
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 02:27:11 -0500

    Display API response in designated HTML element
    Updated the send method to display the response in an element with ID 'h1n'.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -99,6 +99,7 @@ export default{
     async send(){
       const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector("#prompt").value})});
       const data=await response.json(); this.response=data.reply; alert("RES00: "+JSON.stringify(data)); alert("RES01: "+this.response); //console.log(data.message.content);
+      document.querySelector("#h1n").innerText=this.response;
     },
     async sendX(){
       const response=await fetch("/api/ws",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector("#t").value})});
