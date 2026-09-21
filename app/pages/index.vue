31d291634efddd2a86f49c0d0cd7b127b523b4d2
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 01:24:29 -0500

    Update prompt assignment to use querySelector

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -59,7 +59,7 @@ onMounted(()=>{
 fetchU(pr);
   
     //document.querySelector("#prompt").value=document.getElementById("tr").innerText;
-const prompt=pr; //document.querySelector("#prompt").value;                   //const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
+const prompt=document.querySelector("#prompt").value;                   //const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
   //document.getElementById("h1n").innerText=document.getElementById("response").innerText;
   //},5800);
 });
