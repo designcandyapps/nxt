61eb635d9c3d0dcc672a0adbfa38a77ae151e2f8
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 07:46:22 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,8 +2,9 @@
 const {data:page}=await useAsyncData('index',()=>queryContent('/').findOne());
 useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,description:page.value.description,ogDescription:page.value.description});
 import {ref,onMounted} from "vue"; import {converter,differenceEuclidean,formatHex,nearest} from "culori";
-const prompt=ref("");
-const prompt=document.getElementById("prompt").value;
+//const prompt=ref("");
+//const prompt=document.getElementById("prompt").value;
+
 //const pr=ref(""); const pr2=ref(""); const uUrl=ref(""); const pUrl=ref(""); //const proxyUrl=ref("");
 //const prompt=document.querySelector("#prompt"); const pr=document.querySelector("#pr"); const pr2=document.querySelector("#pr2");
 const pr=ref(""); const pr2=ref(""); const uUrl=ref(""); const pUrl=ref("");
