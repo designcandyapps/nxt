894108bf171f3b8de565eb8da0163d02241210ea
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 04:53:08 -0500

    Fix URL assignment in index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -47,7 +47,7 @@ onMounted(()=>{
     //fetchGetty(pr).then(image=>{pho2.value=image.display_sizes[0].uri});
 
 
-    //uUrl.value="pinfluents.com"; pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
+    uUrl.value="pinfluents.com"; pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
     pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
     fetchU(pUrl.value); //.then();
 
