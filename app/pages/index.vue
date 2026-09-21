653bfb8623a6df6f72663ca3957b12be72a5b952
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 10:11:27 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -36,11 +36,11 @@ const generatePalette=async()=>{alert(9);
   //prompt.value=document.querySelector("#prompt").value;
   prompt.value=document.getElementById("tr").innerText; alert("PR99: "+prompt.value);
   //pr.value=document.querySelector("#pr").value; pr2.value=document.querySelector("#pr2").value;
-               // uUrl.value=document.getElementById("sp").innerText;
-               // pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
+                uUrl.value=document.getElementById("sp").innerText;
+                pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
                 //pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
                 //alert("pUrl: "+pUrl.value);
-               // fetchU(uUrl.value);
+                fetchU(uUrl.value);
 
   imageUrl.value=document.getElementById("ee").src; alert("IU1: "+imageUrl.value);
   isLoading.value=true; proxyUrl.value=`/api/proxy?url=${encodeURIComponent(imageUrl.value)}`;
@@ -65,8 +65,8 @@ function discoverPalettes(colors){const palettes={}; for(const color of colors){
 function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 
 onMounted(()=>{alert(8);
-  uUrl.value=document.getElementById("sp").innerText;
-  fetchU(document.getElementById("sp").innerText);
+  //>>>>>uUrl.value=document.getElementById("sp").innerText;
+  //>>>>>fetchU(document.getElementById("sp").innerText);
   //const prompt=document.querySelector("#prompt").value;
 
   setTimeout(function(){
