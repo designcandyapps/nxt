82f4a6aa5d343dbda3a39b5501505b4c7a7df9c0
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 10:14:51 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -33,14 +33,12 @@ const fetchU=async(query)=>{
 
 
 const generatePalette=async()=>{alert(9);
-  //prompt.value=document.querySelector("#prompt").value;
   prompt.value=document.getElementById("tr").innerText; alert("PR99: "+prompt.value);
-  //pr.value=document.querySelector("#pr").value; pr2.value=document.querySelector("#pr2").value;
-                uUrl.value=document.getElementById("sp").innerText;
-                pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
-                //pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
-                //alert("pUrl: "+pUrl.value);
-                fetchU(uUrl.value);
+  uUrl.value=document.getElementById("sp").innerText;
+  pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
+  //pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
+  //alert("pUrl: "+pUrl.value);
+  fetchU(uUrl.value);
 
   imageUrl.value=document.getElementById("ee").src; alert("IU1: "+imageUrl.value);
   isLoading.value=true; proxyUrl.value=`/api/proxy?url=${encodeURIComponent(imageUrl.value)}`;
@@ -67,7 +65,6 @@ function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 onMounted(()=>{alert(8);
   //>>>>>uUrl.value=document.getElementById("sp").innerText;
   //>>>>>fetchU(document.getElementById("sp").innerText);
-  //const prompt=document.querySelector("#prompt").value;
 
   setTimeout(function(){
     generatePalette();
