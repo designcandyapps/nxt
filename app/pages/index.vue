250225385b9857b70ebd15169e0ffad4313b2308
Erin Dunn Halligan <erin@designcandy.com>
2026-09-20 21:57:03 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -34,12 +34,6 @@ const fetchU=async(query)=>{
   //},2800);
   return data.results;
 };
-const pr:string=new URLSearchParams(location.search).get("u")??"wordpress.com"; //document.querySelector("h1").innerText;
-document.getElementById("tr").innerText=pr; document.getElementById("prompt").value=document.getElementById("tr").innerText;
-//const prompt=document.querySelector("#prompt").value; const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
-//pr=!pr||pr==""?document.querySelector("#prompt").value:pr;
-alert("PR: "+pr);
-fetchU(pr);
 onMounted(()=>{
   //setTimeout(function(){
     //<<const pho=document.querySelector("#pho"); const pho2=document.querySelector("#pho2");
@@ -54,11 +48,17 @@ onMounted(()=>{
   //},5800);
   //setTimeout(function(){
 
+  const pr:string=new URLSearchParams(location.search).get("u")??"wordpress.com"; //document.querySelector("h1").innerText;
+  document.getElementById("tr").innerText=pr; document.getElementById("prompt").value=document.getElementById("tr").innerText;
+  //const prompt=document.querySelector("#prompt").value; const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
+  //pr=!pr||pr==""?document.querySelector("#prompt").value:pr;
+  alert("PR: "+pr);
+  fetchU(pr);
 
-  setTimeout(function(){
+  //setTimeout(function(){
     document.querySelector("#prompt").value=document.getElementById("tr").innerText;
     const prompt=document.querySelector("#prompt").value; const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
-  },2300);
+  //},2300);
 });
 </script>
 
@@ -81,10 +81,10 @@ onMounted(()=>{
 export default{
   data(){return{prompt:"",response:null}},
   mounted(){
-    setTimeout(function(){
+    //setTimeout(function(){
       //document.querySelector("h1").innerText=h1; document.querySelector("#prompt").value=h1;
       this.send()
-    },8800);
+    //},8800);
   },
   methods:{
     async send(){
