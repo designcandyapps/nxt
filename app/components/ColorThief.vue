35cf4b7e88cd3b5ea95b85b9661192ee034a71cf
Erin Dunn Halligan <erin@designcandy.com>
2026-09-20 22:01:06 -0500

    Update ColorThief.vue

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -5,7 +5,7 @@ const backgroundImage=ref(""); const toLCH=converter("lch"); const isLoading=ref
 const generatePalette=async()=>{alert(1);
   imageUrl.value=document.getElementById("ee").src; alert("IU1: "+imageUrl.value);
   isLoading.value=true; proxyUrl.value=`/api/proxy?url=${encodeURIComponent(imageUrl.value)}`;
-  const img=new Image(); img.crossOrigin="Anonymous"; img.src=proxyUrl.value; alert("PU2: "+proxyUrl.value);
+  const img=new Image(); img.crossOrigin="Anonymous"; img.src=proxyUrl.value; //alert("PU2: "+proxyUrl.value);
   img.onload=()=>{const colorThief=new ColorThief(); let colors=colorThief.getPalette(img).map((c)=>toLCH({r:c[0]/255,g:c[1]/255,b:c[2]/255,mode:"rgb"}));
     const palettesz=discoverPalettes(colors); document.getElementById("z").innerHTML=`<span class="content"></span>`;
     var i=0; for(const type of Object.keys(palettesz)){
@@ -28,6 +28,6 @@ function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 onMounted(()=>{alert(0);
   setTimeout(function(){
     generatePalette();
-  },1800);
+  },800);
 });
 </script>
