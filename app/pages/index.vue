839ab018793feccc7aa452aa7f6cc9d21ba809d1
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 06:59:14 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 const {data:page}=await useAsyncData('index',()=>queryContent('/').findOne());
 useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,description:page.value.description,ogDescription:page.value.description});
-//const uUrl=ref(""); const pUrl=ref("");
-//import {ref,onMounted} from "vue";
+import {ref,onMounted} from "vue"; //import {converter,differenceEuclidean,formatHex,nearest} from "culori";
 //const prompt=ref(""); const pr=ref(""); const pr2=ref(""); const uUrl=ref(""); const pUrl=ref(""); //const proxyUrl=ref("");
 //const prompt=document.querySelector("#prompt"); const pr=document.querySelector("#pr"); const pr2=document.querySelector("#pr2");
+
+const pr=ref(""); const pr2=ref(""); const uUrl=ref(""); const pUrl=ref("");
+const imageUrl=ref(""); const proxyUrl=ref(""); const palette=ref([]); const backgroundImage=ref(""); const toLCH=converter("lch"); const isLoading=ref(false);
+
 /*const genTktlr=async()=>{//alert(5);
   const proxyUrl=ref("");
   proxyUrl.value=`/api/proxy?url=${encodeURIComponent(document.querySelector("#pr2").value)}`; //pr2.value
@@ -37,6 +40,50 @@ const fetchU=async(query)=>{
   //alert("PRO: "+document.querySelector("#prompt").value);
   return data.results;
 };
+
+
+
+
+
+const generatePalette=async()=>{alert(9);
+  //prompt.value=document.querySelector("#prompt").value; alert("PR99: "+prompt.value);
+  //pr.value=document.querySelector("#pr").value; pr2.value=document.querySelector("#pr2").value;
+  uUrl.value=document.getElementById("sp").innerText;
+  pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
+  ////pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
+  alert("pUrl: "+pUrl.value);
+  
+  imageUrl.value=document.getElementById("ee").src; alert("IU1: "+imageUrl.value);
+  isLoading.value=true; proxyUrl.value=`/api/proxy?url=${encodeURIComponent(imageUrl.value)}`;
+  const img=new Image(); img.crossOrigin="Anonymous"; img.src=proxyUrl.value; //alert("PU2: "+proxyUrl.value);
+  img.onload=()=>{const colorThief=new ColorThief(); let colors=colorThief.getPalette(img).map((c)=>toLCH({r:c[0]/255,g:c[1]/255,b:c[2]/255,mode:"rgb"}));
+    const palettesz=discoverPalettes(colors); document.getElementById("z").innerHTML=`<span class="content"></span>`;
+    var i=0; for(const type of Object.keys(palettesz)){
+      const paletteWrapper=document.createElement("span"); paletteWrapper.classList.add("palette-colors"); document.querySelector(".content").appendChild(paletteWrapper);
+      paletteWrapper.innerHTML=palettesz[type].colors.reduce((html,color)=>{i++; html+=`<span id="dv${i}" style="background:${formatHex(color)}"></span>`;return html},"");
+    }//alert("Z: "+document.getElementById("z").innerHTML);
+    const scientificColors=discoverPalettes(colors); palette.value=Object.keys(scientificColors).map((type)=>({type,colors:scientificColors[type].colors.map((color)=>({hex:formatHex(color)}))}));
+    backgroundImage.value=`url('${imageUrl.value}')`; isLoading.value=false;
+    const r0=document.querySelector("#dv7").style.backgroundColor; alert("G2: "+r0);
+    const r2=document.querySelector("#dv8").style.backgroundColor;
+    const r3=document.querySelector("#dv10").style.backgroundColor;
+    document.body.style.backgroundColor=r0;
+  };
+  img.onerror=()=>{console.error("Failed to Load"); isLoading.value=false}
+};
+function createScientificPalettes(baseColor){const targetHueSteps={analogous:[0,30,60],triadic:[0,120,240],tetradic:[0,90,180,270],complementary:[0,180],splitComplementary:[0,150,210]}; const palettes={}; for(const type of Object.keys(targetHueSteps)){palettes[type]=targetHueSteps[type].map((step)=>({mode:"lch",l:baseColor.l,c:baseColor.c,h:(baseColor.h+step)%360}))} return palettes}
+function discoverPalettes(colors){const palettes={}; for(const color of colors){const targetPalettes=createScientificPalettes(color); for(const paletteType of Object.keys(targetPalettes)){const palette=[]; for(const targetColor of targetPalettes[paletteType]){const availableColors=colors.filter((c)=>!palette.some((existing)=>isColorEqual(c,existing))); const match=nearest(availableColors,differenceEuclidean("lch"))(targetColor)[0]; palette.push(match)} palettes[paletteType]={colors:palette}}} return palettes}
+function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
+
+onMounted(()=>{alert(8);
+  setTimeout(function(){
+    //fetchU(uUrl.value);
+    generatePalette();
+  },1800);
+});
+
+
+/*
 onMounted(()=>{
   //setTimeout(function(){
     //document.getElementById("tr").innerText=h1;
@@ -59,6 +106,7 @@ onMounted(()=>{
     fetchU(uUrl.value);
   //},5800);
 });
+*/
 </script>
 
 <template>
