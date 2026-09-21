16eb35548ec7c85120f58c4c7d604ba8d2e289d5
Erin Dunn Halligan <erin@designcandy.com>
2026-09-20 20:46:54 -0500

    Change fetch URL to retrieve SVG instead of images

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -21,11 +21,11 @@ async function fetchGetty(query){
   }catch(error){console.error("Error2:",error)}
 }
 const fetchU=async(query)=>{
-  const response=await fetch(`https://web.scraper.workers.dev?url=${encodeURIComponent(query)}&selector=img`);
+  const response=await fetch(`https://web.scraper.workers.dev?url=${encodeURIComponent(query)}&selector=svg`);
   //const response=await fetch(`/api/ws?url=${encodeURIComponent(query)}`);
   const data=await response.json(); alert("RESPz: "+JSON.stringify(data));
   const h1=data.result.h1[0];
-  const sv=data.result.sv[0];
+  const sv=data.result.svg[0];
   //setTimeout(function(){
     //document.getElementById("tr").innerText=h1;
 
