298024229ae86a3c5c8109368b637d526e4cb3d0
Erin Dunn Halligan <erin@designcandy.com>
2026-09-21 02:43:08 -0500

    Refactor prompt value retrieval and alerts

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -29,8 +29,8 @@ const fetchU=async(query)=>{
   //document.querySelector("#prompt").value=h1;
   document.getElementById("tr").innerText=h1;
   document.getElementById("prompt").value=document.getElementById("tr").innerText;
-  const prompt=document.querySelector("#prompt").value; //const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
-  alert("PROMPT: "+prompt);
+  //const prompt=document.querySelector("#prompt").value; //const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
+  alert("PROMPT: "+document.querySelector("#prompt").value);
 
   return data.results;
 };
@@ -67,7 +67,8 @@ onMounted(()=>{
   //alert("PR: "+pr);
 fetchU(pr);
 
-/**x**/
+  const prompt=document.querySelector("#prompt").value; //const pr2=document.querySelector("#pr2").value; //alert("PROMPT: "+prompt); alert("PR2: "+pr2);
+  alert("PROMPT: "+prompt);
 });
 </script>
 
