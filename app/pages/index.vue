4ccb5b6e4be2d7150110597be049592864c22435
Erin Dunn Halligan <erin@designcandy.com>
2026-09-25 09:25:11 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -28,7 +28,7 @@ const fetchU=async(query)=>{
 
 
 
-const generatePalette=async()=>{alert(9);
+const generatePalette=async()=>{alert(1);
   uUrl.value=document.getElementById("sp").innerText; alert("uUrl: "+uUrl.value);
   pUrl.value=`https://web.scraper.workers.dev?url=${encodeURIComponent(uUrl.value)}&selector=h1`;
   //pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`; alert("pUrl: "+pUrl.value);
@@ -56,7 +56,7 @@ function createScientificPalettes(baseColor){const targetHueSteps={analogous:[0,
 function discoverPalettes(colors){const palettes={}; for(const color of colors){const targetPalettes=createScientificPalettes(color); for(const paletteType of Object.keys(targetPalettes)){const palette=[]; for(const targetColor of targetPalettes[paletteType]){const availableColors=colors.filter((c)=>!palette.some((existing)=>isColorEqual(c,existing))); const match=nearest(availableColors,differenceEuclidean("lch"))(targetColor)[0]; palette.push(match)} palettes[paletteType]={colors:palette}}} return palettes}
 function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 
-onMounted(()=>{alert(8);
+onMounted(()=>{alert(0);
   setTimeout(function(){
     //const pho=document.querySelector("#pho"); const pho2=document.querySelector("#pho2");
     //fetchPh(prompt).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small}); /*alert("PH: "+pho.value)*/});
