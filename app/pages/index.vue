610c58685dda0a9681aae47253f28deb45163210
Erin Dunn Halligan <erin@designcandy.com>
2026-09-25 17:09:19 -0500

    Change innerText target from 'tr' to 'hr'

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -20,7 +20,7 @@ const fetchU=async(query)=>{
   const response=await fetch(`https://web.scraper.workers.dev?url=${encodeURIComponent(query)}&selector=h1`);
   //const response=await fetch(`/api/ws?url=${encodeURIComponent(query)}`);
   const data=await response.json(); alert("RESPz: "+JSON.stringify(data));
-  const h1=data.result.h1[0]; document.getElementById("tr").innerText=h1;
+  const h1=data.result.h1[0]; document.getElementById("hr").innerText=h1;
   //prompt.value=document.getElementById("tr").innerText; alert("PR99: "+prompt.value);
   return data.results;
 };
