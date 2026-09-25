4cfec69e82ec0a687c4f90875d31964859a07b58
Erin Dunn Halligan <erin@designcandy.com>
2026-09-25 17:40:21 -0500

    Restore alert for debugging innerHTML of element 'z'
    Re-enable alert for debugging the innerHTML of element 'z'.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -44,7 +44,8 @@ const generatePalette=async()=>{alert(1);
     var i=0; for(const type of Object.keys(palettesz)){
       const paletteWrapper=document.createElement("span"); paletteWrapper.classList.add("palette-colors"); document.querySelector(".content").appendChild(paletteWrapper);
       paletteWrapper.innerHTML=palettesz[type].colors.reduce((html,color)=>{i++; html+=`<span id="dv${i}" style="background:${formatHex(color)}"></span>`;return html},"");
-    }//alert("Z: "+document.getElementById("z").innerHTML);
+    }
+    alert("Z: "+document.getElementById("z").innerHTML);
     const scientificColors=discoverPalettes(colors); palette.value=Object.keys(scientificColors).map((type)=>({type,colors:scientificColors[type].colors.map((color)=>({hex:formatHex(color)}))}));
     backgroundImage.value=`url('${imageUrl.value}')`; isLoading.value=false;
     const r0=document.querySelector("#dv7").style.backgroundColor; alert("G2: "+r0);
