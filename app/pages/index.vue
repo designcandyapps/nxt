9ad3c5251e61f48eead5b3d65edb3f975f63e9d1
Erin Dunn Halligan <erin@designcandy.com>
2026-09-26 08:50:11 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,11 +3,6 @@ const {data:page}=await useAsyncData('index',()=>queryContent('/').findOne());
 useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,description:page.value.description,ogDescription:page.value.description});
 import {converter,differenceEuclidean,formatHex,nearest} from "culori"; const pr=ref(""); const uUrl=ref(""); const pUrl=ref("");
 const imageUrl=ref(""); const proxyUrl=ref(""); const palette=ref([]); const backgroundImage=ref(""); const toLCH=converter("lch"); const isLoading=ref(false);
-async function fetchPh(query){
-  const response=await fetch(`https://api.unsplash.com/search/photos?query=${encodeURIComponent(query)}&client_id=OOBNDpH2xNShX6T9wWV_-9py3NtxfpGT2zMcashaO_o`);
-  const data=await response.json(); alert("RES1P: "+JSON.stringify(data));
-  return data.results;
-}
 
 
 const generatePalette=async()=>{alert(1);
