507e8765b690ab6d0c27bfe3cb53fe99e6aeaa39
Erin Dunn Halligan <erin@designcandy.com>
2026-09-26 05:46:12 -0500

    Update index.vue to show alert and set prompt value

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -24,7 +24,8 @@ const fetchU=async(query)=>{
   //const response=await fetch(`/api/ws?url=${encodeURIComponent(query)}`);
   const data=await response.json(); //alert("RESPz: "+JSON.stringify(data));
   const h1=data.result.h1[0]; document.getElementById("tr").innerText=h1;
-  //prompt.value=document.getElementById("tr").innerText;
+  alert("TR: "+document.getElementById("tr").innerText);
+  prompt.value=document.getElementById("tr").innerText;
   t.value=document.getElementById("tr").innerText;
   return data.results;
 };
