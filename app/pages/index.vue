2a506544b6c43c494b6d8fa8a9314841aabaa4d2
Erin Dunn Halligan <erin@designcandy.com>
2026-09-26 06:49:55 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -55,10 +55,11 @@ function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 
 onMounted(()=>{alert(9);
   const pho=document.querySelector("#pho"); const pho2=document.querySelector("#pho2");
-  fetchPh(prompt).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small}); /*alert("PH: "+pho.value)*/});
+  //fetchPh(prompt).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small}); /*alert("PH: "+pho.value)*/});
   //fetchGetty(prompt).then(image=>{pho2.value=image.display_sizes[0].uri});
 
-  uUrl.value="designcandy.com"; //pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
+  uUrl.value=document.getElementById("et").innerText;
+  //uUrl.value="designcandy.com"; //pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
   //document.getElementById("t").value=uUrl.value;
   fetchU(uUrl.value);
   setTimeout(function(){alert(0);
