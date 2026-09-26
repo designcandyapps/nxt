ae5614b7fc6914ebeefb5888f55eb875994519f1
Erin Dunn Halligan <erin@designcandy.com>
2026-09-26 09:22:29 -0500

    Refactor photo fetching functions and cleanup code

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,6 +5,7 @@ import {converter,differenceEuclidean,formatHex,nearest} from "culori";
 const pr=ref(""); const uUrl=ref(""); const pUrl=ref("");
 const imageUrl=ref(""); const proxyUrl=ref(""); const palette=ref([]);
 const backgroundImage=ref(""); const toLCH=converter("lch"); const isLoading=ref(false);
+
 const fetchPh=async(query)=>{
   const response=await fetch(`https://api.unsplash.com/search/photos?query=${encodeURIComponent(query)}&client_id=OOBNDpH2xNShX6T9wWV_-9py3NtxfpGT2zMcashaO_o`);
   const data=await response.json(); //alert("RES1P: "+JSON.stringify(data));
@@ -12,7 +13,7 @@ const fetchPh=async(query)=>{
 };
 async function fetchPh2(query){
   const response=await fetch(`https://api.unsplash.com/search/photos?query=${encodeURIComponent(query)}&client_id=OOBNDpH2xNShX6T9wWV_-9py3NtxfpGT2zMcashaO_o`);
-  const data=await response.json(); alert("RES1P: "+JSON.stringify(data));
+  const data=await response.json(); //alert("RES1P: "+JSON.stringify(data));
   return data.results;
 }
 async function fetchGetty(query){
@@ -59,17 +60,15 @@ function createScientificPalettes(baseColor){const targetHueSteps={analogous:[0,
 function discoverPalettes(colors){const palettes={}; for(const color of colors){const targetPalettes=createScientificPalettes(color); for(const paletteType of Object.keys(targetPalettes)){const palette=[]; for(const targetColor of targetPalettes[paletteType]){const availableColors=colors.filter((c)=>!palette.some((existing)=>isColorEqual(c,existing))); const match=nearest(availableColors,differenceEuclidean("lch"))(targetColor)[0]; palette.push(match)} palettes[paletteType]={colors:palette}}} return palettes}
 function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 
-onMounted(()=>{alert(9);
-  const pho=document.querySelector("#pho"); const pho2=document.querySelector("#pho2");
-  fetchPh2(prompt).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small});/*alert("PH: "+pho.value)*/});
+//onMounted(()=>{alert(9);
+  //const pho=document.querySelector("#pho"); const pho2=document.querySelector("#pho2");
+  //fetchPh(prompt).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small});/*alert("PH: "+pho.value)*/});
   //fetchGetty(prompt).then(image=>{pho2.value=image.display_sizes[0].uri});
-
-  var uu=document.getElementById("et").innerText; //alert("uu: "+uu);
-  fetchU(uu);
-  setTimeout(function(){alert(0);
+  //var uu=document.getElementById("et").innerText; fetchU(uu);
+  /*setTimeout(function(){alert(0);
     generatePalette();
-  },1800);
-});
+  },1800);*/
+//});
 </script>
 
 <template>
@@ -93,7 +92,7 @@ export default{
   mounted(){
     //setTimeout(function(){
       //alert("Test0");
-      //this.send()
+      this.send3()
     //},12800);
   },
   methods:{
@@ -102,6 +101,25 @@ export default{
       const data=await response.json(); this.response=data.reply; alert("RES00: "+JSON.stringify(data)); alert("RES01: "+this.response); //console.log(data.message.content);
       //document.querySelector("#tr").innerText=this.response;
       //document.querySelector("#h1n").innerText=this.response;
+    },
+    async send2(){
+      const response=await fetch(`https://api.unsplash.com/search/photos?query=${encodeURIComponent(query)}&client_id=OOBNDpH2xNShX6T9wWV_-9py3NtxfpGT2zMcashaO_o`);
+      const data=await response.json(); alert("RES1P: "+JSON.stringify(data));
+      return data.results;
+      //document.querySelector("#tr").innerText=this.response;
+      //document.querySelector("#h1n").innerText=this.response;
+    },
+    async send3(){
+      const response=await fetch(`https://web.scraper.workers.dev?url=${encodeURIComponent(query)}&selector=h1`);
+      //const response=await fetch(`/api/ws?url=${encodeURIComponent(query)}`);
+      const data=await response.json(); //alert("RESPz: "+JSON.stringify(data));
+      const h1=data.result.h1[0]; document.getElementById("tr").innerText=h1;
+      //prompt.value=document.getElementById("tr").innerText;
+      var uu=document.getElementById("et").innerText;
+      t.value=h1; //document.getElementById("et").innerText
+      //uUrl.value="designcandy.com"; //pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
+      document.getElementById("pr").value=uu;
+      return data.results;
     },
     async send9(){
       const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector("#pr2").value})});
