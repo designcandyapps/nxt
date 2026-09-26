8187678d78f1742a5abe8acc27842d0710206439
Erin Dunn Halligan <erin@designcandy.com>
2026-09-26 07:54:39 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,6 +10,11 @@ const fetchPh=async(query)=>{
   const data=await response.json(); //alert("RES1P: "+JSON.stringify(data));
   return data.results;
 };
+async function fetchPh2(query){
+  const response=await fetch(`https://api.unsplash.com/search/photos?query=${encodeURIComponent(query)}&client_id=OOBNDpH2xNShX6T9wWV_-9py3NtxfpGT2zMcashaO_o`);
+  const data=await response.json(); alert("RES1P: "+JSON.stringify(data));
+  return data.results;
+}
 async function fetchGetty(query){
   try{
     const response=await fetch(`https://api.gettyimages.com/v3/search/images?phrase=${encodeURIComponent(query)}&page_size=1`,{method:"GET",headers:{"Api-Key":"ep3mq3jxr4u99m7hy3gzzp3g"}});
@@ -56,8 +61,8 @@ function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 
 onMounted(()=>{alert(9);
   const pho=document.querySelector("#pho"); const pho2=document.querySelector("#pho2");
-  //fetchPh(prompt).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small});/*alert("PH: "+pho.value)*/});
-  fetchGetty(prompt).then(image=>{pho2.value=image.display_sizes[0].uri});
+  fetchPh2(prompt).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small});/*alert("PH: "+pho.value)*/});
+  //fetchGetty(prompt).then(image=>{pho2.value=image.display_sizes[0].uri});
 
   var uu=document.getElementById("et").innerText; //alert("uu: "+uu);
   fetchU(uu);
