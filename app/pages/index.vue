b66d734bbffd1f2d982530a8087da5ab3bbfbc71
Erin Dunn Halligan <erin@designcandy.com>
2026-09-25 20:38:38 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -51,7 +51,7 @@ function createScientificPalettes(baseColor){const targetHueSteps={analogous:[0,
 function discoverPalettes(colors){const palettes={}; for(const color of colors){const targetPalettes=createScientificPalettes(color); for(const paletteType of Object.keys(targetPalettes)){const palette=[]; for(const targetColor of targetPalettes[paletteType]){const availableColors=colors.filter((c)=>!palette.some((existing)=>isColorEqual(c,existing))); const match=nearest(availableColors,differenceEuclidean("lch"))(targetColor)[0]; palette.push(match)} palettes[paletteType]={colors:palette}}} return palettes}
 function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 
-onMounted(()=>{
+onMounted(()=>{alert(9);
   const pho=document.querySelector("#pho"); const pho2=document.querySelector("#pho2");
   fetchPh(prompt).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small}); /*alert("PH: "+pho.value)*/});
   //fetchGetty(pr).then(image=>{pho2.value=image.display_sizes[0].uri});
