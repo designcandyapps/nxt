aef5bfc5ac5c69bb346bbf809ae6db7a23a98764
Erin Dunn Halligan <erin@designcandy.com>
2026-09-26 07:39:59 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,14 +7,14 @@ const imageUrl=ref(""); const proxyUrl=ref(""); const palette=ref([]);
 const backgroundImage=ref(""); const toLCH=converter("lch"); const isLoading=ref(false);
 const fetchPh=async(query)=>{
   const response=await fetch(`https://api.unsplash.com/search/photos?query=${encodeURIComponent(query)}&client_id=OOBNDpH2xNShX6T9wWV_-9py3NtxfpGT2zMcashaO_o`);
-  const data=await response.json(); //alert("RES1P: "+JSON.stringify(data));
+  const data=await response.json(); alert("RES1P: "+JSON.stringify(data));
   return data.results;
 };
 async function fetchGetty(query){
   try{
     const response=await fetch(`https://api.gettyimages.com/v3/search/images?phrase=${encodeURIComponent(query)}&page_size=1`,{method:"GET",headers:{"Api-Key":"ep3mq3jxr4u99m7hy3gzzp3g"}});
     if(!response.ok){throw new Error(`Error1:${response.statusText}`)}
-    const data=await response.json(); //alert("RES2P: "+JSON.stringify(data));
+    const data=await response.json(); alert("RES2P: "+JSON.stringify(data));
     if(data.images&&data.images.length>0){const image=data.images[0];console.log("Im:",image);return image}else{console.log("No ims");return null}
   }catch(error){console.error("Error2:",error)}
 }
@@ -27,7 +27,7 @@ const fetchU=async(query)=>{
   var uu=document.getElementById("et").innerText;
   t.value=h1; //document.getElementById("et").innerText
   //uUrl.value="designcandy.com"; //pUrl.value=`/api/ws?url=${encodeURIComponent(uUrl.value)}`;
-  //document.getElementById("pr").value=uu;
+  document.getElementById("pr").value=uu;
 
   return data.results;
 };
@@ -61,7 +61,6 @@ onMounted(()=>{alert(9);
   //fetchGetty(prompt).then(image=>{pho2.value=image.display_sizes[0].uri});
 
   var uu=document.getElementById("et").innerText; //alert("uu: "+uu);
-
   fetchU(uu);
   setTimeout(function(){alert(0);
     generatePalette();
