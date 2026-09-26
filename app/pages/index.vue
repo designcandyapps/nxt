0110b93e7bb0396d6fd016d2367d321fc2be05b0
Erin Dunn Halligan <erin@designcandy.com>
2026-09-26 03:41:54 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,8 +2,8 @@
 const {data:page}=await useAsyncData('index',()=>queryContent('/').findOne());
 useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,description:page.value.description,ogDescription:page.value.description});
 //import {ref,onMounted} from "vue";
-import {converter,differenceEuclidean,formatHex,nearest} from "culori"; const uUrl=ref(""); const pUrl=ref(""); const imageUrl=ref(""); const proxyUrl=ref("");
-const palette=ref([]); const backgroundImage=ref(""); const toLCH=converter("lch"); const isLoading=ref(false);
+import {converter,differenceEuclidean,formatHex,nearest} from "culori"; //const uUrl=ref(""); const pUrl=ref("");
+const imageUrl=ref(""); const proxyUrl=ref(""); const palette=ref([]); const backgroundImage=ref(""); const toLCH=converter("lch"); const isLoading=ref(false);
 const fetchPh=async(query)=>{
   const response=await fetch(`https://api.unsplash.com/search/photos?query=${encodeURIComponent(query)}&client_id=OOBNDpH2xNShX6T9wWV_-9py3NtxfpGT2zMcashaO_o`);
   const data=await response.json(); //alert("RES1P: "+JSON.stringify(data));
